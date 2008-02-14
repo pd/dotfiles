7c3ebb557a1a4d9dc7692fd6cdffe70da618eb6b
Kyle Hargraves <philodespotos@gmail.com>
2008-02-14 01:06:19 -0600

    dun remember why i forced kyleh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,19 +1,18 @@
-export PS1='kyleh %~ %# '
+_prompt_host=`hostname -s`
+export PS1="$_prompt_host %~ %# "
 
 # executed before printing a prompt
 function precmd {
-  host="kyleh"
   _pwd=`echo $PWD | sed "s,$HOME,~,"`
-  str="$host: $_pwd"
+  str="$_prompt_host: $_pwd"
   iterm_set_tab_label $str
   iterm_set_window_title $str
 }
 
 # executed just after reading a command, before running it
 function preexec {
-  host="kyleh"
   cmd=$(history $HISTCMD | cut -b7-)
-  str="$host: $cmd"
+  str="$_prompt_host: $cmd"
   iterm_set_tab_label $str
   iterm_set_window_title $str
 }
