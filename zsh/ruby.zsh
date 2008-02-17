ee94967a77eb939e3f9e930a6f5bc9aec437a6d2
Kyle Hargraves <philodespotos@gmail.com>
2008-02-17 13:50:49 -0600

    convert rails shortcuts to functions, support merb everywhere too
    the shortcut names don't actually make any sense in merb, but i'm sick
    of typing 'sse' a hundred times before i remember: oh yeah, that's
    not how you launch merb...

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,25 +1,24 @@
-## way too much to type
+## generic shortcuts
 gems="/opt/local/lib/ruby/gems/1.8/gems"
-
-## testing
-alias att='autotest'
-alias rbst='ruby stories/stories/all.rb'
-
-## shortcuts to most common rails commands
-alias sse='./script/server'
-alias sst='./script/story'
-alias sc='./script/console'
 alias gs='gem server'
+alias att='autotest'
 
-## works for both rails and merb-core
+## rails / merb
+## uses the rails command if it's present, otherwise merb
+sse () {
+  ([ -f './script/server' ] && ruby ./script/server $*) || merb $*
+}
+sc () {
+  ([ -f './script/console' ] && ruby ./script/console $*) || merb -i $*
+}
 sg () {
-  if [ -f './script/generate' ]; then
-    ruby ./script/generate $*
-  else
-    merb-gen $*
-  fi
+  ([ -f './script/generate' ] && ruby ./script/generate $*) || merb-gen $*
+}
+sst () {
+  ([ -f './script/story' ] && ruby ./script/story $*) || rake 'story[all]'
 }
 
+## launch textmate using the cwd as the project root
 mt () {
   [ "$PWD" = "$HOME" ] && echo "not in $HOME." && return
   mate .
