3a1fbbd0dbd2486507e21f73bfe53dfdd9ac43ac
Kyle Hargraves <philodespotos@gmail.com>
2008-02-17 10:37:36 -0600

    ruby.zsh: some merb support, reorg a bit

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,16 +1,26 @@
-## ruby/rails misc.
+## way too much to type
+gems="/opt/local/lib/ruby/gems/1.8/gems"
+
+## testing
+alias att='autotest'
+alias rbst='ruby stories/stories/all.rb'
+
+## shortcuts to most common rails commands
 alias sse='./script/server'
 alias sst='./script/story'
 alias sc='./script/console'
-alias sg='./script/generate'
-alias att='autotest'
-alias rbst='ruby stories/all.rb'
 alias gs='gem server'
+
+## works for both rails and merb-core
+sg () {
+  if [ -f './script/generate' ]; then
+    ruby ./script/generate $*
+  else
+    merb-gen $*
+  fi
+}
 
 mt () {
   [ "$PWD" = "$HOME" ] && echo "not in $HOME." && return
   mate .
 }
-
-## way too much to type
-gems="/opt/local/lib/ruby/gems/1.8/gems"
