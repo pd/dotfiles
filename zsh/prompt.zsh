3d2215ee14d32f48034a7ce03d1fe1bb984dbc2b
Kyle Hargraves <philodespotos@gmail.com>
2008-02-20 21:59:17 -0600

    show current git branch in zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,13 +1,22 @@
-export PS1='%m %~ %# '
+## expand %m, %*; expand '$(echo hi)', '${foovar}'
+setopt prompt_percent prompt_subst
 
-# executed before printing a prompt
+_prompt_git_current_branch () {
+  ref=$(git-symbolic-ref HEAD 2>/dev/null) || return
+  echo "(gb: ${ref#refs/heads/}) "
+}
+
+## okay now
+export PS1='%m %~ $(_prompt_git_current_branch)%# '
+
+## executed before printing a prompt
 precmd () {
   str=`print -P '%m: %~'`
   iterm_set_tab_label $str
   iterm_set_window_title $str
 }
 
-# executed just after reading a command, before running it
+## executed just after reading a command, before running it
 preexec () {
   cmd=$(history $HISTCMD | cut -b7-)
   str=`print -P "%m: $cmd"`
