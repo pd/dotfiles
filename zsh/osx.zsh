6d8383edcc8b0373a85bfe969bef3e0c230ec125
Kyle Hargraves <philodespotos@gmail.com>
2008-02-20 22:03:38 -0600

    move precmd/preexec iterm stuff into osx.zsh

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -35,3 +35,18 @@ iterm_set_tab_label () {
 iterm_set_window_title () {
   echo -ne "\e]2;$*\a"
 }
+
+# executed just before printing a prompt
+precmd () {
+  str=`print -P '%m: %~'`
+  iterm_set_tab_label $str
+  iterm_set_window_title $str
+}
+
+# executed just after reading a command, before running it
+preexec () {
+  cmd=$(history $HISTCMD | cut -b7-)
+  str=`print -P "%m: $cmd"`
+  iterm_set_tab_label $str
+  iterm_set_window_title $str
+}
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,18 +8,3 @@ _prompt_git_current_branch () {
 
 ## okay now
 export PS1='%m %~ $(_prompt_git_current_branch)%# '
-
-## executed before printing a prompt
-precmd () {
-  str=`print -P '%m: %~'`
-  iterm_set_tab_label $str
-  iterm_set_window_title $str
-}
-
-## executed just after reading a command, before running it
-preexec () {
-  cmd=$(history $HISTCMD | cut -b7-)
-  str=`print -P "%m: $cmd"`
-  iterm_set_tab_label $str
-  iterm_set_window_title $str
-}
