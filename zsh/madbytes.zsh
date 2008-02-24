ec24c6248205a42de77b29432310b8c31a2c15ce
Kyle Hargraves <philodespotos@gmail.com>
2008-02-24 16:01:38 -0600

    o2st() function to display status of all the olympian projs

--- zsh/madbytes.zsh
+++ zsh/madbytes.zsh
@@ -1,2 +1,26 @@
 ## (c) MadBytes, LLC Exclusively Contracted with Chapter Communications, Inc.
 alias dns1='dig @ns1.madbytes.net'
+
+_single_o2st () {
+  proj=$1
+  echo -n "$proj @ "
+  (cd $proj;
+   git-show --abbrev-commit --pretty=format:'%h' | head -1)
+
+  [[ "ambrosia" = $proj ]] && return
+
+  echo -n "  w/ambrosia @ "
+  (cd $proj/vendor/plugins/ambrosia;
+   git-show --abbrev-commit --pretty=format:'%h' | head -1)
+}
+o2st () {
+  for d in o2 ambrosia olympian; do
+    [[ ! -d $d ]] && echo "Can't find $d repository" && return
+  done
+
+  _single_o2st ambrosia
+  echo
+  _single_o2st o2
+  echo
+  _single_o2st olympian
+}
