78e7c22e6f8b136be2da4df3615068c7ceddbd6e
Kyle Hargraves <philodespotos@gmail.com>
2008-04-07 00:51:56 -0500

    use one of the crazy, wayTMI&Colours prompts that are in vogue

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,8 +3,11 @@ setopt prompt_percent prompt_subst
 
 _prompt_git_current_branch () {
   ref=$(git-symbolic-ref HEAD 2>/dev/null) || return
-  echo "(gb: ${ref#refs/heads/}) "
+  echo "(gb: $fg[green]${ref#refs/heads/}$fg[default]) "
 }
 
 ## okay now
-export PS1='%m %~ $(_prompt_git_current_branch)%# '
+# export PS1='%m %~ $(_prompt_git_current_branch)%# '
+export PS1='
+-- $fg[yellow][$fg[default] %n @ %m $fg[yellow]]$fg[default] $fg[yellow][$fg[default] $fg[magenta]%~ $fg[yellow]]$fg[default] $(_prompt_git_current_branch)$fg[yellow][$fg[default] %D{%a, %b %d %T} $fg[yellow]]$fg[default] 
+-- %(?||(exit: $fg[red]%?$fg[default]%) )%# '
