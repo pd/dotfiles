351a96f67ed34054b3b4014c56345aa4d622248a
Kyle Hargraves <philodespotos@gmail.com>
2008-04-19 09:10:20 -0500

    don't need merb aliases; alias ss='script/spec'

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -8,18 +8,10 @@ path=(~/gems/bin $path)
 
 ## rails / merb
 ## uses the rails command if it's present, otherwise merb
-sse () {
-  ([ -f './script/server' ] && ruby ./script/server $*) || merb $*
-}
-sc () {
-  ([ -f './script/console' ] && ruby ./script/console $*) || merb -i $*
-}
-sg () {
-  ([ -f './script/generate' ] && ruby ./script/generate $*) || merb-gen $*
-}
-sst () {
-  ([ -f './script/story' ] && ruby ./script/story $*) || rake 'story[all]'
-}
+alias ss='./script/spec -O spec/spec.opts'
+alias sse='./script/server'
+alias sc='./script/console'
+alias sg='./script/generate'
 alias edge-rails='ruby ~/vendor/rails/railties/bin/rails'
 
 ## launch textmate using the cwd as the project root
