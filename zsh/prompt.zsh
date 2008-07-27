5a288efc9d1f85fe1ca780c7b305406897b6364e
Kyle Hargraves <philodespotos@gmail.com>
2008-07-26 21:54:06 -0500

    rm stale bg job detection in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -33,5 +33,5 @@ preexec () {
 
 ## okay now
 export PS1='
--- %{$fg_bold[blue]%}[%{$reset_color%} %n @ %m %{$fg[green]%}%~ %{$fg_bold[blue]%}]%{$reset_color%} $(_prompt_git_current_branch)%{$fg_bold[blue]%}[%{$reset_color%} %D{%a, %b %d %T} %{$fg_bold[blue]%}]%{$reset_color%}%(1j. !! has a job.)
+-- %{$fg_bold[blue]%}[%{$reset_color%} %n @ %m %{$fg[green]%}%~ %{$fg_bold[blue]%}]%{$reset_color%} $(_prompt_git_current_branch)%{$fg_bold[blue]%}[%{$reset_color%} %D{%a, %b %d %T} %{$fg_bold[blue]%}]%{$reset_color%}
 -- %(?||(\$!: %{$fg[red]%}%?%{$reset_color%}%) )%# '
