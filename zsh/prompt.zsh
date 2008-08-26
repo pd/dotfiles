6d96b2eab58862540b8ab5234493cad493674786
Kyle Hargraves <philodespotos@gmail.com>
2008-08-26 09:50:49 -0500

    Lots more git info in prompt
    Stolen from ryanb's dotfiles at github
    
      http://github.com/ryanb/dotfiles

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,37 +1,58 @@
 ## expand %m, %*; expand '$(echo hi)', '${foovar}'
 setopt prompt_percent prompt_subst
 
-_prompt_git_current_branch () {
-  ref=$(git symbolic-ref HEAD 2>/dev/null) || return
-  echo "(gb: $fg[green]${ref#refs/heads/}$reset_color) "
-}
+_prompt_git_info () {
+  local g="$(git rev-parse --git-dir 2>/dev/null)"
+  if [ -n "$g" ]; then
+    local r
+    local b
+    if [ -d "$g/../.dotest" ]
+    then
+      if test -f "$g/../.dotest/rebasing"
+      then
+        r=" | REBASE"
+      elif test -f "$g/../.dotest/applying"
+      then
+        r=" | AM"
+      else
+        r=" | AM/REBASE"
+      fi
+      b="$(git symbolic-ref HEAD 2>/dev/null)"
+    elif [ -f "$g/.dotest-merge/interactive" ]
+    then
+      r=" | REBASE-i"
+      b="$(cat "$g/.dotest-merge/head-name")"
+    elif [ -d "$g/.dotest-merge" ]
+    then
+      r=" | REBASE-m"
+      b="$(cat "$g/.dotest-merge/head-name")"
+    elif [ -f "$g/MERGE_HEAD" ]
+    then
+      r=" | MERGING"
+      b="$(git symbolic-ref HEAD 2>/dev/null)"
+    else
+      if [ -f "$g/BISECT_LOG" ]
+      then
+        r=" | BISECTING"
+      fi
+      if ! b="$(git symbolic-ref HEAD 2>/dev/null)"
+      then
+        if ! b="tag: $(git describe --exact-match HEAD 2>/dev/null)"
+        then
+          b="$(cut -c1-7 "$g/HEAD")..."
+        fi
+      fi
+    fi
 
-## iterm
-##   \e]1 = tab label
-##   \e]2 = window title
-iterm_set_tab_label () {
-  echo -ne "\e]1;$*\a"
-}
-iterm_set_window_title () {
-  echo -ne "\e]2;$*\a"
-}
-
-# executed just before printing a prompt
-precmd () {
-  str=`print -P '%m: %~'`
-  iterm_set_tab_label $str
-  iterm_set_window_title $str
-}
-
-# executed just after reading a command, before running it
-preexec () {
-  cmd=$(history $HISTCMD | cut -b7-)
-  str=`print -P "%m: $cmd"`
-  iterm_set_tab_label $str
-  iterm_set_window_title $str
+    if [ -n "$1" ]; then
+      printf "$1" "${b##refs/heads/}$r"
+    else
+      printf "($fg[green]%s$reset_color) " "${b##refs/heads/}$r"
+    fi
+  fi
 }
 
 ## okay now
 export PS1='
--- %{$fg_bold[blue]%}[%{$reset_color%} %n @ %m %{$fg[green]%}%~ %{$fg_bold[blue]%}]%{$reset_color%} $(_prompt_git_current_branch)%{$fg_bold[blue]%}[%{$reset_color%} %D{%a, %b %d %T} %{$fg_bold[blue]%}]%{$reset_color%}
+-- %{$fg_bold[blue]%}[%{$reset_color%} %n @ %m %{$fg[green]%}%~ %{$fg_bold[blue]%}]%{$reset_color%} $(_prompt_git_info)%{$fg_bold[blue]%}[%{$reset_color%} %D{%a, %b %d %T} %{$fg_bold[blue]%}]%{$reset_color%}
 -- %(?||(\$!: %{$fg[red]%}%?%{$reset_color%}%) )%# '
