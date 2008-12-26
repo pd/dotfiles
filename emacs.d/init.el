70f72f297284781cf1ada328b39f2f162ca20b90
Kyle Hargraves <philodespotos@gmail.com>
2008-12-26 11:13:34 -0600

    customize should save to custom.el, always use ruby-electric

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,6 +7,11 @@
 (add-to-list 'load-path dotfiles-dir)
 (add-to-list 'load-path (concat dotfiles-dir "/vendor"))
 
+; Make sure Custom doesn't write to this friggin file.
+(setq custom-file (concat dotfiles-dir "/custom.el"))
+
+; Libraries the emacs-starter-kit assures me I always want. Cursory
+; overview suggests it's generally the case.
 (require 'cl)
 (require 'saveplace)
 (require 'ffap)
@@ -27,18 +32,25 @@
 (global-set-key (kbd "M-SPC") 'set-mark-command) ; Quicksilver is C-SPC
 
 ;;;; Things only to be loaded on demand
+
 ; ruby mode
 (autoload 'ruby-mode "ruby-mode"
   "Minor mode for ruby" t)
 (add-to-list 'auto-mode-alist '("\\.(rb|rake)$" . ruby-mode))
+(add-hook 'ruby-mode-hook
+ '(lambda ()
+    (require 'ruby-electric)))
 
-(autoload 'git-blame-mode "git-blame"
-  "Minor mode for incremental blame for Git" t)
-
-; haml / sass
 (autoload 'haml-mode "haml-mode"
   "Minor mode for HAML files" t)
 (autoload 'sass-mode "sass-mode"
   "Minor mode for Sass files" t)
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 (add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
+
+(autoload 'yaml-mode "yaml-mode"
+  "Minor mode for YAML files" t)
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
+
+(autoload 'git-blame-mode "git-blame"
+  "Minor mode for incremental blame for Git" t)
