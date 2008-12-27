a4457a7ded0155c32baf0439638408f98d34c5f0
Kyle Hargraves <philodespotos@gmail.com>
2008-12-26 23:04:44 -0600

    support plugins/liboly/ in jump-to-spec-file
    I'd love to start refactoring this list but my elisp-fu is way
    too weak.

--- emacs.d/my-jumps.el
+++ emacs.d/my-jumps.el
@@ -8,13 +8,18 @@
 (defjump 'jump-to-spec-file
   '(("app/\\1/\\2.rb" . "spec/\\1/\\2_spec.rb")
     ("app/\\1/\\2/\\3.rb" . "spec/\\1/\\2/\\3_spec.rb")
-    ("lib/\\1.rb"     . "spec/lib/\\1_spec.rb"))
+    ("lib/\\1.rb"     . "spec/lib/\\1_spec.rb")
+    ("plugins/\\1/app/\\2/\\3.rb" . "plugins/\\1/spec/\\2/\\3_spec.rb")
+    ("plugins/\\1/app/\\2/\\3.rb" . "plugins/\\1/spec/\\2/integration/\\3_spec.rb"))
   'project-root-or-current-directory
   "Jump from an implementation to its spec")
 
+; I really just want the jump specs here to be the ALIST from above,
+; but with key/value swapped. But an hour of attempts ended in failure.
 (defjump 'jump-to-implementation-file
   '(("spec/\\1/\\2/\\3_spec.rb" . "app/\\1/\\2/\\3.rb")
-    ("spec/\\1/\\2_spec.rb" . "app/\\1/\\2.rb"))
+    ("spec/\\1/\\2_spec.rb" . "app/\\1/\\2.rb")
+    ("spec/lib/\\1_spec.rb" . "lib/\\1.rb"))
   'project-root-or-current-directory
   "Jump from a spec to its implementation")
 
