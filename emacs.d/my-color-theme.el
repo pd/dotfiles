afe5f01f1258402baf1b5f38f044d321dbe60c9a
Kyle Hargraves <philodespotos@gmail.com>
2008-12-27 01:28:58 -0600

    switch color theme to dark blue2
    and mention all the other themes that weren't completely intolerable.

--- emacs.d/my-color-theme.el
+++ emacs.d/my-color-theme.el
@@ -1,6 +1,14 @@
 (require 'color-theme)
 (setq color-theme-is-cumulative nil)
 (color-theme-initialize)
-(color-theme-deep-blue)
+(color-theme-dark-blue2)
+
+; Other decent themes so far:
+; (color-theme-deep-blue)
+; (color-theme-hober)
+; (color-theme-snow)
+; (color-theme-vim-colors)
+; (color-theme-pok-wob) ; white on black
+; (color-theme-pok-wog) ; white on grey
 
 (provide 'my-color-theme)
