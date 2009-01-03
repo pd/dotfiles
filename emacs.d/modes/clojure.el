b6383add03d45a858cdb147553003319916aa139
Kyle Hargraves <philodespotos@gmail.com>
2009-01-03 13:17:30 -0600

    got a real slime-repl again
    Modified submodules:
    
    * vendor/slime bfc8b2f...3349c5e (29):
      > Fix merge.
      > slime-mrepl.el: new file
      > Delete junk.
      > Experimental implementation of "channels".
      > Experimental channels
      > * slime.el ([test] arglist): Guard against nil.  ECL
      > Mention the missing REPL.
      > 	Arglists of user-defined types are now displayed by slime-autodoc
      > 	* swank-loader.lisp (*contribs*): Add `swank-sbcl-exts'.
      > 	* slime.el (slime-eval-async, slime-dispatch-event): Canoncalize
      > 	* slime-autodoc.el: Autodoc is now implemented on top of ElDoc.
      > * swank-openmcl.lisp (frame-source-location-for-emacs)
      > For buffers without filename, map the name of the tempfile back to
      > * swank.lisp (sleep-for): New function
      > 	* swank-arglists.lisp (format-arglist-for-echo-area): Catch errors
      > 	* swank-sbcl-exts.lisp: Wrap file in a big #+sbcl (PROGN ...), so
      > Fix automatic merge.
      > * swank.lisp (maybe-redirect-global-io): Don't consider
      > * swank-openmcl.lisp ([method] source-locations (symbol)): Drop
      > * slime.el (slime-cd, slime-pwd): New commands.
      > * slime.el ([test] find-definition.2): Also fails for Lispworks.
      > 	* slime-repl.lisp (slime-repl-set-package): Set
      > 	As of now, `C-u C-c C-c' compiled a function with maximum debug
      > 	* slime-sbcl-exts.el, swank-sbcl-exts.lisp: New contrib.
      > 	* swank-arglists.lisp (defstruct arglist-dummy): Remove
      > * swank-openmcl.lisp (swank-compile-string, compile-temp-file): Use
      > * swank-openmcl.lisp (find-definitions, source-locations): Use
      > Recent CCLs support much better source location recording.
      > * swank-openmcl.lisp (function-source-location): Use
    
    * vendor/swank-clojure f5a2c31...f5a4919 (1):
      > more correct create-repl

--- emacs.d/modes/clojure.el
+++ emacs.d/modes/clojure.el
@@ -2,8 +2,13 @@
 (require 'swank-clojure-autoload)
 (require 'slime-autoloads)
 
+(slime-setup '(slime-repl))
+
 (swank-clojure-config
- (setq swank-clojure-jar-path "/Users/kyleh/vendor/clojure/clojure.jar"))
+ (setq swank-clojure-binary "~/bin/clj")
+ (setq swank-clojure-jar-path "~/.clj/clojure.jar")
+ (add-to-list 'swank-clojure-extra-classpaths "~/sandbox/clojoku/src")
+ (add-to-list 'swank-clojure-extra-classpaths "~/sandbox/clojoku/test"))
 
 (add-hook 'clojure-mode-hook
           (lambda ()
