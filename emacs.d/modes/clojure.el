232bef6a3af08b208dfa0e1668fed107654b9619
Kyle Hargraves <philodespotos@gmail.com>
2009-01-06 11:00:36 -0600

    adjust clojoku paths; C-c b for slime-eval-buffer
    C-c C-b was taken by slime-interrupt-lisp (iirc)

--- emacs.d/modes/clojure.el
+++ emacs.d/modes/clojure.el
@@ -7,10 +7,10 @@
 (swank-clojure-config
  (setq swank-clojure-binary "~/bin/clj")
  (setq swank-clojure-jar-path "~/.clj/clojure.jar")
- (add-to-list 'swank-clojure-extra-classpaths "~/sandbox/clojoku/src")
- (add-to-list 'swank-clojure-extra-classpaths "~/sandbox/clojoku/test"))
+ (add-to-list 'swank-clojure-extra-classpaths "~/clj/clojoku/src")
+ (add-to-list 'swank-clojure-extra-classpaths "~/clj/clojoku/test"))
 
 (add-hook 'clojure-mode-hook
           (lambda ()
-            (define-key clojure-mode-map (kbd "C-c C-b") 'slime-eval-buffer)
+            (define-key clojure-mode-map (kbd "C-c b") 'slime-eval-buffer)
             (setq indent-tabs-mode nil)))
