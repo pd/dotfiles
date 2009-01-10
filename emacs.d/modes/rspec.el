a135fd9d7008c8935d52535b93e819dff523eda3
Kyle Hargraves <philodespotos@gmail.com>
2009-01-09 23:37:23 -0600

    autoload rspec-mode when it makes sense

--- emacs.d/modes/rspec.el
+++ emacs.d/modes/rspec.el
@@ -1,3 +1,8 @@
+(autoload 'rspec-mode "rspec-mode" "Major mode for rspec" t)
+(autoload 'rspec-run-suite "rspec-mode" "" t)
+
+(add-to-list 'auto-mode-alist '("_spec.rb\\'" . rspec-mode))
+
 (add-hook 'rspec-mode-hook
           (lambda ()
             (define-key rspec-mode-map (kbd "C-c s s") 'rspec-run-suite)
