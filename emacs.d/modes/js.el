6d0b915dd284dedcaf0f275e97127c27dca1a67d
Kyle Hargraves <philodespotos@gmail.com>
2009-01-13 10:42:49 -0600

    trying to disable js2-mode's *egregious* autoindents

--- emacs.d/modes/js.el
+++ emacs.d/modes/js.el
@@ -1,6 +1,10 @@
 (autoload 'js2-mode "js2-mode" "Major mode for JS files" t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(eval-after-load 'js2-mode
+  (progn
+    (setq js2-electric-keys '())))
+
 (add-hook 'js2-mode-hook
           (lambda ()
-            (coding-hook)
-            (define-key js2-mode-map (kbd "<return>") 'newline-and-indent)))
+            (coding-hook)))
