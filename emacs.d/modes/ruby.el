319e1958d28c51a183ff18c5a963d964c5b48241
Kyle Hargraves <philodespotos@gmail.com>
2009-02-21 15:29:21 -0600

    M-p, M-n finally work in inf-ruby

--- emacs.d/modes/ruby.el
+++ emacs.d/modes/ruby.el
@@ -6,8 +6,12 @@
 
 (add-hook 'ruby-mode-hook
           (lambda ()
-            (require 'ruby-electric)
             (coding-hook)
             (define-key ruby-mode-map (kbd "C-c j s")  'rspec-jump-to-spec-file)
             (define-key ruby-mode-map (kbd "C-c j i")  'rspec-jump-to-implementation-file)
             (define-key ruby-mode-map (kbd "<return>") 'newline-and-indent)))
+
+(add-hook 'inferior-ruby-mode-hook
+          (lambda ()
+            (setq inferior-ruby-prompt-pattern "^>> *"
+                  inferior-ruby-first-prompt-pattern "^>> *")))
