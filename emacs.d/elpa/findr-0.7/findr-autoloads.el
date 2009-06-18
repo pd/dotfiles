597f24897caf09bb2b4b3f02c2459a89c25397b2
Kyle Hargraves <pd@krh.me>
2009-06-17 22:15:30 -0500

    just track all of emacs.d/elpa i guess

---
+++ emacs.d/elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,16 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19001 45170
+;;;;;;  137241))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; findr-autoloads.el ends here
---
+++ emacs.d/elpa/yasnippet-bundle-0.5.10/yasnippet-bundle-pkg.el
@@ -0,0 +1,1 @@
+(define-package "yasnippet-bundle" "0.5.10" "Yet another snippet extension (Auto compiled bundle)" (quote nil))
