7a22f0da074737f2226c1ab89aaec58ca6897359
Kyle Hargraves <pd@krh.me>
2009-07-02 11:15:23 -0500

    meh i don't use snippets ever

--- emacs.d/elpa/yasnippet-bundle-0.5.10/yasnippet-bundle-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; yasnippet-bundle-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "yasnippet-bundle" "yasnippet-bundle.el" (19000
-;;;;;;  13832))
-;;; Generated autoloads from yasnippet-bundle.el
-(require 'yasnippet-bundle)
-
-;;;***
-
-;;;### (autoloads nil nil ("yasnippet-bundle-pkg.el") (19000 13832
-;;;;;;  425417))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yasnippet-bundle-autoloads.el ends here
--- emacs.d/elpa/yasnippet-bundle-0.5.10/yasnippet-bundle-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "yasnippet-bundle" "0.5.10" "Yet another snippet extension (Auto compiled bundle)" (quote nil))
