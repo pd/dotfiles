8245df16fdd3cabda7865fdb2be903ef18adc7cd
Kyle Hargraves <pd@krh.me>
2009-08-20 11:46:34 -0500

    xmonad: dzen status bar, use a color scheme

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,20 +5,41 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Themes
+import XMonad.Util.Loggers
 import XMonad.Layout.TabBarDecoration
 
 -- bound in xinitrc to alt_r
 myModMask = mod3Mask
 
+myDzenPP h = defaultPP
+   { ppOutput = hPutStrLn h
+   , ppCurrent = dzenColor "#77507b" "#000" . wrap "<" "*>"
+   , ppHidden = dzenColor "#855c1b" "#000" . wrap "" "*"
+   , ppHiddenNoWindows = dzenColor "#855c1b" "black"
+   , ppExtras = [ date "%a %b %d  %I:%M %p"
+                , loadAvg
+                , battery
+                ]
+   , ppWsSep = " "
+   , ppSep = " | "
+   , ppTitle = shorten 45
+   , ppOrder = \(ws:l:t:exs) -> [t,l,ws] ++ exs
+   }
+
+myDzenFont = "'-*-terminus-*-*-*-*-14-*-*-*-*-*-iso8859'"
+myDzenCommand = "dzen2 -bg '#000' -fg '#555753' -fn " ++ myDzenFont
+
 main = do
-    h <- spawnPipe ("dzen2")
+    dzen <- spawnPipe myDzenCommand
     xmonad $ defaultConfig
         { modMask = myModMask
-        , workspaces = ["emacs", "web", "irc", "sh", "min"]
+        , workspaces = ["web", "sauce", "irc", "sh", "min"]
         , focusFollowsMouse = False
         , terminal = "urxvt"
-        , borderWidth = 2
-        , logHook = dynamicLogWithPP byorgeyPP { ppOutput = hPutStrLn h }
+        , borderWidth = 1
+        , normalBorderColor = "#555753"
+        , focusedBorderColor = "#729fcf"
+        , logHook = dynamicLogWithPP (myDzenPP dzen)
         , manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         }
