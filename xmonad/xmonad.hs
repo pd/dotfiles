aa2d3bf27a2806d906d296e16a55e922069a67ff
Kyle Hargraves <pd@krh.me>
2009-08-21 21:08:48 -0500

    pretty xmonad status bar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,36 +1,40 @@
-import System.IO
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Themes
 import XMonad.Util.Loggers
+
 import XMonad.Layout.TabBarDecoration
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+
+import System.IO
+import Data.Char
 
 -- bound in xinitrc to alt_r
 myModMask = mod3Mask
 
-myDzenPP h = defaultPP
-   { ppOutput = hPutStrLn h
-   , ppCurrent = dzenColor "#855c1b" "#000" . wrap "<" "*>"
-   , ppHidden = dzenColor "grey" "#000" . wrap "" "*"
-   , ppHiddenNoWindows = dzenColor "grey" "#000"
-   , ppExtras = [ date "%a %b %d  %I:%M %p"
-                , loadAvg
-                , battery
-                ]
-   , ppWsSep = " "
-   , ppSep = " | "
-   , ppTitle = shorten 45
-   , ppOrder = \(ws:l:t:exs) -> [t,l,ws] ++ exs
+-- Log hook
+myLogHook h = dynamicLogWithPP $ defaultPP
+   { ppOutput           = \s -> hPutStrLn h (" " ++ s)
+   , ppCurrent          = dzenColor "#855c1b" "#000" . wrap "" "*"
+   , ppHidden           = dzenColor "grey" "#000"
+   , ppHiddenNoWindows  = dzenColor "grey" "#000"
+   , ppLayout = map toLower . wrap "(" ")"
+   , ppWsSep  = " "
+   , ppSep    = "  "
+   , ppTitle  = shorten 45
+   , ppOrder  = \(ws:l:t:exs) -> [ws,l,t]++exs
    }
 
-myDzenFont = "'-*-terminus-*-*-*-*-14-*-*-*-*-*-iso8859'"
-myDzenCommand = "dzen2 -bg '#000' -fg 'grey' -fn " ++ myDzenFont
+myDzenFont = "-*-liberation mono-medium-r-*-*-14-*-*-*-*-*-*-*"
+myXMonadBarCommand = "dzen2 -ta l -bg '#000' -fg 'grey' -fn '" ++ myDzenFont ++ "'"
+myStatsBarCommand = "dzen2 -bg '#000' -fg 'grey' -fn " ++ myDzenFont
 
 main = do
-    dzen <- spawnPipe myDzenCommand
+    xmonadBar <- spawnPipe myXMonadBarCommand
+    statsBar <- spawnPipe myStatsBarCommand
     xmonad $ defaultConfig
         { modMask = myModMask
         , workspaces = ["web", "sauce", "irc", "sh", "min"]
@@ -39,7 +43,7 @@ main = do
         , borderWidth = 1
         , normalBorderColor = "#555753"
         , focusedBorderColor = "#729fcf"
-        , logHook = dynamicLogWithPP (myDzenPP dzen)
+        , logHook = myLogHook xmonadBar
         , manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         }
