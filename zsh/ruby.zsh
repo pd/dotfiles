ff23f67f62677d40ff53305741c1c2d89cfdd8b6
Kyle Hargraves <pd@krh.me>
2009-09-04 00:11:02 -0500

    ruby.zsh should set up gems and rip

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,25 +1,15 @@
-## generic shortcuts
-alias asp='RSPEC=1 autotest'
-alias att='autotest'
-
-## use gems from ~/gems, easier to maintain
-if [ -d ~/gems ]; then
+## use gems from ~/.gem or ~/gems if present
+if [ -d ~/.gem ]; then
+  export GEM_HOME=~/.gem
+  path=(~/.gem/bin $path)
+elif [ -d ~/gems ]; then
   export GEM_HOME=~/gems
   path=(~/gems/bin $path)
 fi
 
-## rails / merb
-## uses the rails command if it's present, otherwise merb
-alias ss='./script/spec -O spec/spec.opts'
-alias sse='./script/server'
-alias sc='./script/console'
-alias sg='./script/generate'
-alias sdb='./script/dbconsole'
-alias edge-rails='ruby ~/vendor/rails/railties/bin/rails'
-alias sl='ruby ~/oly-dev/script/launcher'
-
-## launch textmate using the cwd as the project root
-mt () {
-  [ "$PWD" = "$HOME" ] && echo "not in $HOME." && return
-  mate .
-}
+## rip
+if [ -d ~/.rip ]; then
+  export RIPDIR=~/.rip
+  export RUBYLIB=$RIPDIR/active/lib
+  path=(~/.rip/active/bin $path)
+fi
