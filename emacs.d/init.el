f7edd94df3893e35bf9de6766b7850fa16c34486
Kyle Hargraves <pd@krh.me>
2009-12-24 22:43:07 -0600

    ignore tramp's connection history log

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -216,6 +216,9 @@
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")))
 
+; tramp
+(setq tramp-default-method "ssh")
+
 ; smex. should always come last.
 (require 'smex)
 (smex-initialize)
