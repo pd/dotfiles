8055ed55d1facb09100fa31af3c06c6a5eaf50fb
Kyle Hargraves <pd@krh.me>
2010-04-14 15:00:22 -0500

    path += /usr/share/java/apache-ant/bin

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -1,4 +1,9 @@
 alias bb='bauerbill'
 
 # funky place to put bins but okay ...
-path=(/usr/bin/perlbin/core /usr/bin/perlbin/vendor $path)
+path=(
+  /usr/bin/perlbin/core
+  /usr/bin/perlbin/vendor
+  /usr/share/java/apache-ant/bin
+  $path
+)
