e2e49d86b929cd9d92da5d123898b043370408b8
Kyle Hargraves <pd@krh.me>
2010-04-15 12:28:08 -0500

    newline-and-indent in js mode

--- emacs.d/pd/js.el
+++ emacs.d/pd/js.el
@@ -2,7 +2,11 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'" . espresso-mode))
 
+(defun pd/add-js-bindings ()
+  (pd/enable-newline-and-indent espresso-mode-map))
+
 (setq espresso-indent-level 2)
 (add-hook 'espresso-mode-hook 'pd/run-coding-hook)
+(add-hook 'espresso-mode-hook 'pd/add-js-bindings)
 
 (provide 'pd/js)
