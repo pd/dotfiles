b29fc23391968b91eb2adc82559409555cd3fdec
Kyle Hargraves <pd@krh.me>
2010-04-15 05:11:13 -0500

    run-coding-hook from espresso-mode-hook

--- emacs.d/pd/js.el
+++ emacs.d/pd/js.el
@@ -1,6 +1,8 @@
 (autoload 'espresso-mode "espresso" "Major mode for javascript" t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'" . espresso-mode))
+
 (setq espresso-indent-level 2)
+(add-hook 'espresso-mode-hook 'pd/run-coding-hook)
 
 (provide 'pd/js)
