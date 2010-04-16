b260c1ea94aee29b3b32ee3669ea95de08355881
Kyle Hargraves <pd@krh.me>
2010-04-16 01:15:22 -0500

    color-theme-despot; just do it all myself.

--- emacs.d/pd/theme.el
+++ emacs.d/pd/theme.el
@@ -1,23 +1,68 @@
 (require 'color-theme)
-(setq color-theme-history-max-length t) ; unlimited
+
+(setq color-theme-history-max-length t
+      color-theme-is-cumulative nil)
+
 (pd/load-directory "~/.emacs.d/vendor/themes" nil 'nomessage)
 
-(defun pd/theme-tweak-erc ()
-  (eval-after-load 'erc-stamp
-    '(progn
-       (set-face-foreground 'erc-input-face "light steel blue")
-       (set-face-foreground 'erc-my-nick-face "steel blue")
-       (set-face-foreground 'erc-timestamp-face "grey50"))))
+(defun color-theme-despot ()
+  "color-theme for pd. mostly inkpot."
+  (interactive)
+  (color-theme-install
+   '(color-theme-despot
 
-(defun pd/theme-tweak-magit ()
-  (eval-after-load 'magit
-    '(progn
-       (set-face-background 'magit-item-highlight "gray12")
-       (set-face-foreground 'magit-diff-add "green3")
-       (set-face-foreground 'magit-diff-del "red3"))))
+     ((foreground-color . "#cfbfad")
+      (background-color . "#000000")
+      (border-color . "#3e3e5e")
+      (cursor-color . "#404040")
+      (background-mode . dark))
 
-(color-theme-inkpot)
-(pd/theme-tweak-erc)
-(pd/theme-tweak-magit)
+     (region ((t (:background "#303030"))))
+     (highlight ((t (:background "#005bd4"))))
+     (hl-line ((t (:background "#1f1f1b"))))
+     (fringe ((t (:background "#090909"))))
+
+     (font-lock-builtin-face ((t (:foreground "#c080d0"))))
+     (font-lock-comment-face ((t (:foreground "#708090" :italic t))))
+     (font-lock-constant-face ((t (:foreground "#506dbd"))))
+     (font-lock-doc-face ((t (:foreground "#cd8b00"))))
+     (font-lock-function-name-face ((t (:foreground "#87cefa"))))
+     (font-lock-keyword-face ((t (:bold t :foreground "#c080d0"))))
+     (font-lock-preprocessor-face ((t (:foreground "309090"))))
+     (font-lock-reference-face ((t (:bold t :foreground "#808bed"))))
+     (font-lock-string-face ((t (:foreground "#ffcd8b"))))
+     (font-lock-type-face ((t (:foreground "#ff8bff"))))
+     (font-lock-variable-name-face ((t nil)))
+     (font-lock-warning-face ((t (:foreground "#ffffff" :background "#ff0000"))))
+
+     (comint-highlight-prompt ((t (:foreground "steel blue"))))
+     (comint-highlight-input ((t (:italic t :bold t))))
+
+     (minibuffer-prompt ((t (:bold t :foreground "steel blue"))))
+
+     (isearch ((t (:bold t :foreground "#090909" :background "#3e3e5e"))))
+
+     (modeline ((t (:bold t :foreground "#b9b9b9" :background "#3e3e5e"))))
+     (modeline-inactive ((t (:bold t :foreground "#708090" :background "#303030"))))
+
+     (show-paren-match-face ((t (:foreground "steel blue"))))
+
+     (magit-item-highlight ((t (:background "gray12"))))
+     (magit-diff-add ((t (:foreground "green3"))))
+     (magit-diff-del ((t (:foreground "red3"))))
+
+     (erc-input-face ((t (:foreground "light steel blue"))))
+     (erc-my-nick-face ((t (:foreground "steel blue"))))
+     (erc-timestamp-face ((t (:foreground "grey50"))))
+
+     )))
+
+(defun pd/set-color-theme (&optional frame)
+  (let ((color-theme-is-global nil))
+    (select-frame frame)
+    (when window-system
+      (color-theme-despot))))
+
+(add-hook 'after-make-frame-functions 'pd/set-color-theme)
 
 (provide 'pd/theme)
