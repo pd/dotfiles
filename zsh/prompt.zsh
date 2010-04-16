da840651dff90c9d5bc1e0d6f05b7705fd93d570
Kyle Hargraves <pd@krh.me>
2010-04-15 23:41:09 -0500

    even simpler

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,4 +4,4 @@ setopt prompt_percent prompt_subst
 ## emacs shell == "dumb"
 # disable zsh line editing and let emacs do it
 [[ $TERM = "dumb" ]] && unsetopt zle
-export PS1='%~%(?|| (\$!: %{$fg[red]%}%?%{$reset_color%}%)) %# '
+export PS1='%~ » '
