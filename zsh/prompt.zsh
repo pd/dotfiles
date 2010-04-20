b2830e8b1ccecc945d3875f05396648b9b6e9360
Kyle Hargraves <pd@krh.me>
2010-04-20 03:01:57 -0500

    when connected via ssh, show hostname in PS1

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,4 +4,8 @@ setopt prompt_percent prompt_subst
 ## emacs shell == "dumb"
 # disable zsh line editing and let emacs do it
 [[ $TERM = "dumb" ]] && unsetopt zle
-export PS1='%~ » '
+if [[ -n $SSH_CONNECTION ]]; then
+  export PS1='%~ @ %m » '
+else
+  export PS1='%~ » '
+fi
