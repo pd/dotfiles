cf8a2417b700f3efc7663a54184c1ed13a4b189c
Kyle Hargraves <pd@krh.me>
2010-04-21 12:11:21 -0500

    no need for a wrapping defun

--- emacs.d/pd/shell.el
+++ emacs.d/pd/shell.el
@@ -2,10 +2,7 @@
 
 (setq shell-prompt-pattern "^[^\n]*[#$%>»] *")
 
-(defun pd/enable-ansi-color-for-comint ()
-  (ansi-color-for-comint-mode-on))
-
-(add-hook 'shell-mode-hook 'pd/enable-ansi-color-for-comint)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (add-hook 'shell-mode-hook 'pd/turn-off-show-trailing-whitespace)
 
 (provide 'pd/shell)
