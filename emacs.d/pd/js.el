8c378a49506181693dc5268639d30f0e59cb0dc1
Kyle Hargraves <pd@krh.me>
2010-05-02 03:15:17 -0500

    espresso: stop reindenting just cuz i hit comma

--- emacs.d/pd/js.el
+++ emacs.d/pd/js.el
@@ -5,8 +5,13 @@
 (defun pd/add-js-bindings ()
   (pd/enable-newline-and-indent espresso-mode-map))
 
+(defun pd/espresso-reindent-less ()
+  (define-key espresso-mode-map (kbd ",") 'self-insert-command)
+  (define-key espresso-mode-map (kbd ";") 'self-insert-command))
+
 (setq espresso-indent-level 2)
 (add-hook 'espresso-mode-hook 'pd/run-coding-hook)
 (add-hook 'espresso-mode-hook 'pd/add-js-bindings)
+(add-hook 'espresso-mode-hook 'pd/espresso-reindent-less)
 
 (provide 'pd/js)
