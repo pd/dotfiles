c0a0bdecdc0968dc6d773d330e2760f8a7236ccf
Kyle Hargraves <pd@krh.me>
2010-05-08 07:39:44 -0500

    emacs 23.2: espresso now ships with emacs as js-mode

--- emacs.d/pd/js.el
+++ emacs.d/pd/js.el
@@ -1,17 +1,12 @@
-(autoload 'espresso-mode "espresso" "Major mode for javascript" t)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . espresso-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js-mode))
 
 (defun pd/add-js-bindings ()
-  (pd/enable-newline-and-indent espresso-mode-map))
+  (pd/enable-newline-and-indent js-mode-map))
 
-(defun pd/espresso-reindent-less ()
-  (define-key espresso-mode-map (kbd ",") 'self-insert-command)
-  (define-key espresso-mode-map (kbd ";") 'self-insert-command))
+(setq js-indent-level 2
+      js-auto-indent-flag nil)
 
-(setq espresso-indent-level 2)
-(add-hook 'espresso-mode-hook 'pd/run-coding-hook)
-(add-hook 'espresso-mode-hook 'pd/add-js-bindings)
-(add-hook 'espresso-mode-hook 'pd/espresso-reindent-less)
+(add-hook 'js-mode-hook 'pd/run-coding-hook)
+(add-hook 'js-mode-hook 'pd/add-js-bindings)
 
 (provide 'pd/js)
