f54e6d9adcc191918990e379260419b1b495cc05
Kyle Hargraves <pd@krh.me>
2010-05-11 08:27:20 -0500

    C-c x l: slime
    aka "run-lisp"; i don't know why it magically defaults to clojure
    for me. i expected to need to configure that. life is good.

--- emacs.d/pd/bindings.el
+++ emacs.d/pd/bindings.el
@@ -47,6 +47,7 @@
 (global-set-key (kbd "C-c x r") 'run-ruby)
 (global-set-key (kbd "C-c x p") 'run-python)
 (global-set-key (kbd "C-c x h") 'run-haskell)
+(global-set-key (kbd "C-c x l") 'slime) ; how this defaults to clojure i dunno
 
 ; misc
 (global-set-key (kbd "C-c s") 'shell)
