5a9e008bfb2110a0651c3b72f7fd59a46c430101
Kyle Hargraves <pd@krh.me>
2010-05-12 03:42:48 -0500

    mozrepl support

--- emacs.d/pd/js.el
+++ emacs.d/pd/js.el
@@ -9,4 +9,10 @@
 (add-hook 'js-mode-hook 'pd/run-coding-hook)
 (add-hook 'js-mode-hook 'pd/add-js-bindings)
 
+(defun pd/enable-moz-minor-mode ()
+  (moz-minor-mode 1))
+
+(autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
+(add-hook 'js-mode-hook 'pd/enable-moz-minor-mode)
+
 (provide 'pd/js)
