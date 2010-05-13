d1ab5b9c5b18f2a20f414cab7d8292c3aa5e4597
Kyle Hargraves <pd@krh.me>
2010-05-13 04:27:15 -0500

    shell-mode C-c d: shell-resync-dirs

--- emacs.d/pd/shell.el
+++ emacs.d/pd/shell.el
@@ -2,7 +2,11 @@
 
 (setq shell-prompt-pattern "^[^\n]*[#$%>»] *")
 
+(defun pd/enable-shell-mode-bindings ()
+  (define-key shell-mode-map (kbd "C-c d") 'dirs))
+
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (add-hook 'shell-mode-hook 'pd/turn-off-show-trailing-whitespace)
+(add-hook 'shell-mode-hook 'pd/enable-shell-mode-bindings)
 
 (provide 'pd/shell)
