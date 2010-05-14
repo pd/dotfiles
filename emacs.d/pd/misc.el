f51db7b5b256995944546ae10751dd07c161e578
Kyle Hargraves <pd@krh.me>
2010-05-14 01:15:57 -0500

    open *ruby* and *js* in the same window

--- emacs.d/pd/misc.el
+++ emacs.d/pd/misc.el
@@ -1,4 +1,8 @@
 ; why does woman open its own frame by default?
 (setq woman-use-own-frame nil)
 
+; run-ruby etc should put the buffer in my current window,
+; not a seemingly random different one ...
+(nconc same-window-buffer-names '("*ruby*" "*js*"))
+
 (provide 'pd/misc)
