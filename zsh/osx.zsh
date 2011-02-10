2713b751a64e0c1ef5125372cb1832234bda2600
Kyle Hargraves <pd@krh.me>
2011-02-10 10:05:00 -0600

    emacs on osx should be Emacs.app not /usr/local/bin/...

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -5,6 +5,9 @@ clear-xattr () {
   done
 }
 
+# emacs from terminal should not launch homebrew's funky version
+alias emacs='open /Applications/Emacs.app'
+
 ## finder window in current directory
 ## maybe some day
 alias finder='open -a finder .'
