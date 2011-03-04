b83da7e9596a3b65e40b706a3b0118fb4cdfede0
Kyle Hargraves <pd@krh.me>
2011-03-04 07:17:39 -0600

    favor yas' ido-prompt. ido4president.

--- emacs.d/pd/yasnippet.el
+++ emacs.d/pd/yasnippet.el
@@ -1,8 +1,8 @@
 (when (package-installed-p 'yasnippet-bundle)
-  (setq yas/root-directory "~/.emacs.d/snippets")
+  (setq yas/root-directory "~/.emacs.d/snippets"
+        yas/prompt-functions '(yas/ido-prompt
+                               yas/x-prompt
+                               yas/completing-prompt))
   (yas/load-directory yas/root-directory))
-
-(defun 'pd/turn-on-yasnippet ()
-  (yas/minor-mode t))
 
 (provide 'pd/yasnippet)
