ec199c4647c605eb7631f0a05627351136c6c7f9
Kyle Hargraves <pd@krh.me>
2011-03-10 11:59:36 -0600

    back to ensime, now with my paragraph movement unfucked =)

--- emacs.d/pd/scala.el
+++ emacs.d/pd/scala.el
@@ -1,10 +1,13 @@
 (require 'scala-mode-auto)
+(require 'ensime)
 
 (defvar pd/scala-hook nil)
 
 (defun pd/add-scala-mode-keybindings ()
   (pd/enable-newline-and-indent scala-mode-map)
-  (pd/restore-paragraph-movement scala-mode-map))
+  (pd/restore-paragraph-movement ensime-mode-map)
+  (define-key ensime-mode-map (kbd "C-c C-p") 'ensime-backward-note)
+  (define-key ensime-mode-map (kbd "C-c C-n") 'ensime-forward-note))
 
 (add-hook 'pd/scala-hook 'pd/run-coding-hook)
 (add-hook 'pd/scala-hook 'pd/turn-on-show-paren-mode)
@@ -14,5 +17,6 @@
   (run-hooks 'pd/scala-hook))
 
 (add-hook 'scala-mode-hook 'pd/run-scala-hook)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
 (provide 'pd/scala)
