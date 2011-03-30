ab92c0b45d77c565979d0c665e3ede44d8d0dc58
Kyle Hargraves <pd@krh.me>
2011-03-30 13:27:01 -0500

    terms named *zsh*, C-c s takes me back to latest term as shell did

--- emacs.d/pd/shell.el
+++ emacs.d/pd/shell.el
@@ -9,12 +9,27 @@
 (add-hook 'shell-mode-hook 'pd/turn-off-show-trailing-whitespace)
 (add-hook 'shell-mode-hook 'pd/enable-shell-mode-bindings)
 
-(setq term-prompt-regexp "^[^\n]*[#$%>»] *")
+(setq term-prompt-regexp "^[^\n]*[#$%>»] *"
+      term-ansi-buffer-base-name t)
 (add-hook 'term-mode-hook 'ansi-color-for-comint-mode-on)
 (add-hook 'term-mode-hook 'pd/turn-off-show-trailing-whitespace)
 
-(defun pd/term ()
-  (interactive)
-  (ansi-term (or (getenv "SHELL") "/bin/zsh")))
+(defun pd/term-buffer-name ()
+  (find-if (lambda (b)
+             (save-excursion
+               (set-buffer b)
+               (string= "term-mode" (symbol-name major-mode))))
+           (buffer-list)))
+
+(defun pd/term-buffer-exists ()
+  (not (eq ()
+           (pd/term-buffer-name))))
+
+(defun pd/term (create-new)
+  (interactive "P")
+  (let ((current-term (pd/term-buffer-name)))
+    (if (and (current-term) (not create-new))
+        (switch-to-buffer current-term)
+      (ansi-term (or (getenv "SHELL") "/bin/zsh")))))
 
 (provide 'pd/shell)
