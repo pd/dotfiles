299ace2c76eee400e24d8e334d14f9dd688a032d
Kyle Hargraves <pd@krh.me>
2011-03-30 14:40:12 -0500

    bring back browser selector magic in emacs

--- emacs.d/pd/misc.el
+++ emacs.d/pd/misc.el
@@ -1,6 +1,9 @@
 ; how to open urls
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "chromium")
+      browse-url-generic-program (cond ((pd/macosx-p) "open")
+                                       ((executable-find "chromium") "chromium")
+                                       ((executable-find "chromium-dev") "chromium-dev")
+                                       t "firefox"))
 
 ; why does woman open its own frame by default?
 (setq woman-use-own-frame nil)
