df218decb8953521d6c357e91037a859aafbfe0c
Kyle Hargraves <pd@krh.me>
2011-03-31 11:23:11 -0500

    meh just autoload yaml-mode myself, dunno what's with elpa

--- emacs.d/pd/yaml.el
+++ emacs.d/pd/yaml.el
@@ -1,6 +1,7 @@
 (defun pd/add-yaml-keybindings ()
   (define-key yaml-mode-map (kbd "C-c C-p") 'yaml-path/path))
 
+(autoload 'yaml-mode "yaml-mode")
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
 
