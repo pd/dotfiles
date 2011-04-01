0e88280ff4eaef4ba4b90ae2d5484891e8a05d51
Kyle Hargraves <pd@krh.me>
2011-04-01 11:49:51 -0500

    i haven't used chrome.el in ages

--- emacs.d/pd/chrome.el
+++
@@ -1,8 +0,0 @@
-; emacs chrome server lets me edit a textfield's content from emacs
-; but only boot it if this is emacsd
-(if (and (daemonp) (locate-library "edit-server"))
-    (progn
-      (require 'edit-server)
-      (edit-server-start)))
-
-(provide 'pd/chrome)
