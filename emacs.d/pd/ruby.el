f3e54345d5951250e6d0fbcd4e547777aa9de441
Kyle Hargraves <pd@krh.me>
2011-04-09 22:29:16 -0500

    update inf-ruby prompt patterns to match rvm's custom irb prompt style

--- emacs.d/pd/ruby.el
+++ emacs.d/pd/ruby.el
@@ -4,6 +4,9 @@
 (add-hook 'ruby-mode-hook 'pd/run-coding-hook)
 (add-hook 'ruby-mode-hook 'pd/add-ruby-keybindings)
 
+(setq inf-ruby-first-prompt-pattern "^.*[0-9:]+ *[>*\"'] *"
+      inf-ruby-prompt-pattern "\\(^\\(irb(.*)[0-9:]+[>*\"'] *\\)+\\)\\|\\(^.*:[0-9]+.*> *\\)")
+
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
