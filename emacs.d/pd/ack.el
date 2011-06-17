7a8fc3682afc5effdd2392529967f83517592f70
Kyle Hargraves <pd@krh.me>
2011-06-17 11:11:15 -0500

    just always load full-ack hey why not

--- emacs.d/pd/ack.el
+++ emacs.d/pd/ack.el
@@ -1,9 +1,8 @@
 ; ack should search all files, and ignore case. i am lazy.
-(eval-after-load 'full-ack
-  '(progn
-     (setq ack-ignore-case t
-           ack-arguments '("-a")
-           ack-executable (or (executable-find "ack")
-                              (executable-find "ack-grep")))))
+(require 'full-ack)
+(setq ack-ignore-case t
+      ack-arguments '("-a")
+      ack-executable (or (executable-find "ack")
+                         (executable-find "ack-grep")))
 
 (provide 'pd/ack)
