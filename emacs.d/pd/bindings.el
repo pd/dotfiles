0c04db55dd320775c7746e2ea7cc304f6f672d69
Kyle Hargraves <pd@krh.me>
2011-06-17 11:11:23 -0500

    C-c f = 'ffap

--- emacs.d/pd/bindings.el
+++ emacs.d/pd/bindings.el
@@ -65,12 +65,13 @@
 ; misc
 (global-set-key (kbd "M-+") 'pd/increase-font-size)
 (global-set-key (kbd "M-_") 'pd/decrease-font-size)
+(global-set-key (kbd "C-c f") 'ffap)
 (global-set-key (kbd "C-c s") 'shell)
 (global-set-key (kbd "C-c t") 'pd/term)
 (global-set-key (kbd "<f6>") 'linum-mode)
-(global-set-key (kbd "C-h a") 'apropos) ; defaults to command-apropos
 (global-set-key (kbd "C-c m") 'woman)
 (global-set-key (kbd "C-c y") 'x-clipboard-yank)
+(global-set-key (kbd "C-h a") 'apropos) ; defaults to command-apropos
 
 ; mac only
 (when (pd/macosx-p)
