31b4048c7694acba8bd3634497079513932ad116
Kyle Hargraves <pd@krh.me>
2011-09-26 18:38:01 -0500

    trying out el-get

--- emacs.d/pd/packages.el
+++ emacs.d/pd/packages.el
@@ -24,4 +24,15 @@
   (add-path "~/dotfiles/vendor/workgroups")
   (add-path "~/dotfiles/vendor/cucumber.el"))
 
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
+(el-get 'sync)
+
 (provide 'pd/packages)
