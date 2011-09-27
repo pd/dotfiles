64fed786dbdb734f8d372a952aa69d9f5526f270
Kyle Hargraves <pd@krh.me>
2011-09-26 23:04:43 -0500

    rm emacs.d/elpa

--- emacs.d/elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19001 45170
-;;;;;;  137241))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; findr-autoloads.el ends here
--- emacs.d/elpa/inflections-1.0/inflections-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; inflections-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (pluralize-string singularize-string) "inflections"
-;;;;;;  "inflections.el" (19001 45169))
-;;; Generated autoloads from inflections.el
-
-(autoload (quote singularize-string) "inflections" "\
-Not documented
-
-\(fn STR)" nil nil)
-
-(autoload (quote pluralize-string) "inflections" "\
-Not documented
-
-\(fn STR)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("inflections-pkg.el") (19001 45169 354425))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; inflections-autoloads.el ends here
