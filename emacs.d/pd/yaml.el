d5f429551171e7384065c372fd7705be8e4a033e
Kyle Hargraves <pd@krh.me>
2011-10-04 10:51:44 -0500

    rm yaml-path stuffs

--- emacs.d/pd/yaml.el
+++ emacs.d/pd/yaml.el
@@ -1,12 +1,5 @@
-(defun pd/add-yaml-keybindings ()
-  (define-key yaml-mode-map (kbd "C-c C-p") 'yaml-path/path))
-
 (autoload 'yaml-mode "yaml-mode")
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
-
-(autoload 'yaml-path/path "yaml-path")
-
-(add-hook 'yaml-mode-hook 'pd/add-yaml-keybindings)
 
 (provide 'pd/yaml)
