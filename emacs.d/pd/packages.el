bf422a816a00ccaf9ea26049fe19fee04bbd658c
Kyle Hargraves <pd@krh.me>
2011-10-12 15:34:35 -0500

    fix feature-mode key in el-get-sources

--- emacs.d/pd/packages.el
+++ emacs.d/pd/packages.el
@@ -40,7 +40,7 @@
                :url "git://github.com/pd/haml-mode.git"
                :branch "wip")
 
-        (:after feature-mode
+        (:name feature-mode
                 :after (lambda ()
                          (setq org-table-number-fraction 2))) ; impossible, thus never right-align.
 
