ad85337dcfdbaf29439eaf64f242ba8aa1d84a48
Kyle Hargraves <pd@krh.me>
2011-10-15 11:07:49 -0500

    add mactex's crazy bin path to $PATH

--- shell_profile/path.sh
+++ shell_profile/path.sh
@@ -11,6 +11,7 @@ add_to_path_if_exists /usr/bin/perlbin/vendor
 add_to_path_if_exists /usr/bin/core_perl
 add_to_path_if_exists /usr/lib/perl5/vendor_perl/bin
 add_to_path_if_exists /usr/share/java/apache-ant/bin
+add_to_path_if_exists /usr/local/texlive/2011/bin/x86_64-darwin
 add_to_path_if_exists /opt/java/bin
 add_to_path_if_exists ~/.cabal/bin
 add_to_path_if_exists ~/local/bin
