ce16f212c02239435727d976b3db77f9623206ee
Kyle Hargraves <pd@krh.me>
2011-11-28 14:50:20 -0600

    rabl = ruby

--- emacs.d/pd/ruby.el
+++ emacs.d/pd/ruby.el
@@ -8,7 +8,7 @@
       inf-ruby-prompt-pattern "\\(^\\(irb(.*)[0-9:]+[>*\"'] *\\)+\\)\\|\\(^.*:[0-9]+.*> *\\)")
 
 (dolist (regexp '("\\.rake$" "Rakefile$" "Guardfile$"
-                  "Gemfile$" "\\.gemspec$" "\\.?irbrc$"))
+                  "Gemfile$" "\\.gemspec$" "\\.?irbrc$" "\\.rabl$"))
   (add-to-list 'auto-mode-alist (cons regexp 'ruby-mode)))
 
 (provide 'pd/ruby)
