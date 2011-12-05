de0666483574605b43ce0f9504007d8bd8a74b0c
Kyle Hargraves <pd@krh.me>
2011-12-05 01:04:11 -0600

    close comint completions buffers automatically

--- emacs.d/pd/misc.el
+++ emacs.d/pd/misc.el
@@ -39,4 +39,32 @@
 ; not a seemingly random different one ...
 (nconc same-window-buffer-names '("*ruby*" "*js*"))
 
+; http://snarfed.org/automatically_close_completions_in_emacs_shell_comint_mode
+(defun comint-close-completions ()
+  "Close the comint completions buffer.
+Used in advice to various comint functions to automatically close
+the completions buffer as soon as I'm done with it. Based on
+Dmitriy Igrishin's patched version of comint.el."
+  (if comint-dynamic-list-completions-config
+      (progn
+        (set-window-configuration comint-dynamic-list-completions-config)
+        (setq comint-dynamic-list-completions-config nil))))
+
+(defadvice comint-send-input (after close-completions activate)
+  (comint-close-completions))
+
+(defadvice comint-dynamic-complete-as-filename (after close-completions activate)
+  (if ad-return-value (comint-close-completions)))
+
+(defadvice comint-dynamic-simple-complete (after close-completions activate)
+  (if (member ad-return-value '('sole 'shortest 'partial))
+      (comint-close-completions)))
+
+(defadvice comint-dynamic-list-completions (after close-completions activate)
+    (comint-close-completions)
+    (if (not unread-command-events)
+        ;; comint's "Type space to flush" swallows space. put it back in.
+        (setq unread-command-events (listify-key-sequence " "))))
+
+
 (provide 'pd/misc)
