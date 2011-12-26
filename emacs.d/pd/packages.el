b68f144f0bad9a81d74e3574d6676d8d42f8e604
Kyle Hargraves <pd@krh.me>
2011-12-26 16:44:03 -0600

    Just give up, i'll never use emacschrome

--- emacs.d/pd/packages.el
+++ emacs.d/pd/packages.el
@@ -41,7 +41,7 @@
                 coffee-mode
                 scala-mode ;ensime
                 clojure-mode paredit elein
-                eredis emacschrome)
+                eredis)
        (mapcar 'el-get-source-name el-get-sources)))
 
 (el-get 'sync pd/el-get-packages)
