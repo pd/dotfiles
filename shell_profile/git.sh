2af918063b70fdaa449c86cea3f4b4d265b50e1b
Kyle Hargraves <pd@krh.me>
2012-06-01 03:29:42 -0500

    rm old git aliases

--- shell_profile/git.sh
+++ shell_profile/git.sh
@@ -3,29 +3,3 @@ if [ -f /usr/local/etc/bash_completion.d/git_completion.bash ]; then
 fi
 
 alias g=git
-
-# alias gst='git status'
-# alias gba='git branch -a'
-# alias gbav='gba -v'
-
-# alias gl='git log'
-
-# alias gco='git checkout'
-# alias gci='git commit'
-# alias gb='git branch'
-# alias gm='git merge'
-# alias grb='git rebase'
-
-# alias gpl='git pull'
-# alias gps='git push'
-# alias gf='git fetch'
-# alias gru='git remote update'
-# alias gcp='git cherry-pick'
-
-# alias gsh='git show'
-# alias gd='git diff'
-# alias gds='git diff --cached'
-# alias gdhd='git diff HEAD'
-# alias gdst='git diff --stat'
-
-# alias gsm='git submodule'
