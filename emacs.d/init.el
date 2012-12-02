84afcd153ed55feaf4ca50cf1ab4e405d08232f5
Kyle Hargraves <pd@krh.me>
2012-12-02 14:50:00 -0600

    Strip down init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,30 +10,33 @@
 (require 'pd/coding)
 (require 'pd/bindings)
 (require 'pd/ido)
-(require 'pd/packages)
-(require 'pd/org)
 
-(require 'pd/shell)
-(require 'pd/tramp)
+; (require 'pd/packages)
+; (require 'pd/org)
+; 
+; (require 'pd/shell)
+; (require 'pd/tramp)
+; (require 'pd/lisps)
+; (require 'pd/ruby)
+; (require 'pd/js)
+; (require 'pd/irc)
+; (require 'pd/smex)
+; (require 'pd/pcmpl-powify)
+; 
+; (require 'pd/misc)
+
+; old crap
 ; (require 'pd/p4)
-(require 'pd/lisps)
-(require 'pd/ruby)
-(require 'pd/js)
 ; (require 'pd/haskell)
 ; (require 'pd/scala)
-(require 'pd/irc)
-(require 'pd/smex)
-(require 'pd/pcmpl-powify)
-
-(require 'pd/misc)
 
 ; private
-(when (pd/has-private-emacsd-p)
-  (pd/load-private-emacsd))
+; (when (pd/has-private-emacsd-p)
+;  (pd/load-private-emacsd))
 
 ; boot the emacs server if it's not available yet
-(require 'server)
-(unless (or (server-running-p) (daemonp))
-  (server-start))
-
-(turn-on-save-visited-files-mode)
+; (require 'server)
+; (unless (or (server-running-p) (daemonp))
+;   (server-start))
+; 
+; (turn-on-save-visited-files-mode)
