f7acbd3f78a41a04a5c8caf6c18e0d25bf5bd1cd
Kyle Hargraves <pd@krh.me>
2012-12-12 13:04:16 -0600

    For now, vendor emacs HEAD ruby-mode.el
    This fixes cucumber steps files that didn't wrap Given/When/Then regexps in
    parens. Holy crap my life is improved 1000x.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,6 +2,10 @@
 
 (add-to-list 'load-path user-emacs-directory)
 
+; I grabbed ruby-mode from emacs HEAD, as I don't really feel like waiting
+; for that to build. This should be in 24.3 when it's released.
+(add-to-list 'load-path (file-name-as-directory (concat user-emacs-directory "vendor")))
+
 ; ELPA.
 (require 'package)
 (setq package-archives
