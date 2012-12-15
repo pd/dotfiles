7a33939f3c6e1ea36f92c6b78ee8409877a14f32
Kyle Hargraves <pd@krh.me>
2012-12-15 16:23:22 -0600

    enable ruby-tools-mode from ruby-mode-hook
    Gives me handy:
    
      C-' / C-" -- swap quote type of string at point
      C-: -- convert string at point to symbol
      C-; -- empty contents of string at point

--- emacs.d/pd/ruby.el
+++ emacs.d/pd/ruby.el
@@ -3,6 +3,7 @@
 
 (add-hook 'ruby-mode-hook 'pd/run-coding-hook)
 (add-hook 'ruby-mode-hook 'pd/add-ruby-keybindings)
+(add-hook 'ruby-mode-hook 'ruby-tools-mode)
 
 (setq inf-ruby-first-prompt-pattern "^.*[0-9:]+ *[>*\"'] *"
       inf-ruby-prompt-pattern "\\(^\\(irb(.*)[0-9:]+[>*\"'] *\\)+\\)\\|\\(^.*:[0-9]+.*> *\\)")
