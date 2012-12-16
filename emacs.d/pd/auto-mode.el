896331aef87f51569913df9702d8381edc49a471
Kyle Hargraves <pd@krh.me>
2012-12-15 18:25:43 -0600

    *.ru = ruby-mode

--- emacs.d/pd/auto-mode.el
+++ emacs.d/pd/auto-mode.el
@@ -1,5 +1,5 @@
 (dolist (regexp '("\\.rake$" "Rakefile$" "Guardfile$"
-                  "Gemfile$" "\\.gemspec$" "\\.?irbrc$" "\\.rabl$"))
+                  "Gemfile$" "\\.gemspec$" "\\.?irbrc$" "\\.rabl$" "\\.ru$"))
   (add-to-list 'auto-mode-alist (cons regexp 'ruby-mode)))
 
 (add-to-list 'auto-mode-alist (cons "\\.md$" 'markdown-mode))
