790c41ad4a24bf7664f65b46c4cc60eb84a9b72c
Kyle Hargraves <pd@krh.me>
2012-12-15 18:29:18 -0600

    elisp: eldoc-mode; M-. is find-function-at-point

--- emacs.d/pd/lisps.el
+++ emacs.d/pd/lisps.el
@@ -12,8 +12,12 @@
   (run-hooks 'pd/lisp-hook))
 
 (add-hook 'emacs-lisp-mode-hook 'pd/run-lisp-hook)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'clojure-mode-hook 'pd/run-lisp-hook)
 (add-hook 'slime-repl-mode-hook 'pd/run-lisp-hook)
+
+; I don't need TAGS when I'm looking for an elisp function.
+(define-key emacs-lisp-mode-map (kbd "M-.") 'find-function-at-point)
 
 ; slime-mode steals M-p and M-n to navigate compilation error
 ; notes. i don't care about those. please give me paragraph
@@ -22,7 +26,6 @@
   (pd/restore-paragraph-movement slime-mode-map))
 
 (add-hook 'slime-mode-hook 'pd/restore-paragraph-movement)
-
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
 
 (provide 'pd/lisps)
