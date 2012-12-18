cfadd199c6f6b217408ec7f169cd9eec2ef54004
Kyle Hargraves <pd@krh.me>
2012-12-18 11:13:46 -0600

    pd/google

--- emacs.d/pd/defuns.el
+++ emacs.d/pd/defuns.el
@@ -244,4 +244,15 @@ on `pd/zsh-dir-aliases-cache'."
           pd/zsh-dir-aliases-cache
           :initial-value path))
 
+; ganked from emacs-prelude
+(defun pd/google ()
+  "Googles a query or region if any."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+         (buffer-substring (region-beginning) (region-end))
+       (read-string "Google: "))))))
+
 (provide 'pd/defuns)
