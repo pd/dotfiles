d5a57c77360c211daf94893188088f15cccaceb5
Kyle Hargraves <pd@krh.me>
2012-12-18 11:13:18 -0600

    defun xmpfilter

--- emacs.d/pd/ruby.el
+++ emacs.d/pd/ruby.el
@@ -8,4 +8,11 @@
 (setq inf-ruby-first-prompt-pattern "^.*[0-9:]+ *[>*\"'] *"
       inf-ruby-prompt-pattern "\\(^\\(irb(.*)[0-9:]+[>*\"'] *\\)+\\)\\|\\(^.*:[0-9]+.*> *\\)")
 
+; xmpfilter
+(defun xmpfilter ()
+  (interactive)
+  (let ((beg (if (region-active-p) (region-beginning) (point-min)))
+        (end (if (region-active-p) (region-end) (point-max))))
+    (shell-command-on-region beg end "xmpfilter" nil 'replace)))
+
 (provide 'pd/ruby)
