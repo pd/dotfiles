edacd04c86970dbe2fe2bb56957a9b3ac1345f85
Kyle Hargraves <pd@krh.me>
2012-12-20 10:37:37 -0600

    subword-mode in ruby

--- emacs.d/pd/ruby.el
+++ emacs.d/pd/ruby.el
@@ -4,6 +4,7 @@
 (add-hook 'ruby-mode-hook 'pd/run-coding-hook)
 (add-hook 'ruby-mode-hook 'pd/add-ruby-keybindings)
 (add-hook 'ruby-mode-hook 'ruby-tools-mode)
+(add-hook 'ruby-mode-hook 'subword-mode)
 
 (setq inf-ruby-first-prompt-pattern "^.*[0-9:]+ *[>*\"'] *"
       inf-ruby-prompt-pattern "\\(^\\(irb(.*)[0-9:]+[>*\"'] *\\)+\\)\\|\\(^.*:[0-9]+.*> *\\)")
