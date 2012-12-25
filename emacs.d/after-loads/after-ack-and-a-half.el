1c93fbdb121eec0516440e502a04f6f5e90fd159
Kyle Hargraves <pd@krh.me>
2012-12-25 02:46:16 -0600

    prefix arg to M-x ack-* prompts for dir

--- emacs.d/after-loads/after-ack-and-a-half.el
+++ emacs.d/after-loads/after-ack-and-a-half.el
@@ -1,4 +1,27 @@
-(defalias 'ack 'ack-and-a-half)
-(defalias 'ack-same 'ack-and-a-half-same)
-(defalias 'ack-find-file 'ack-and-a-half-find-file)
-(defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+;; The default interactive behavior of ack-and-a-half is the opposite
+;; of my workflow.  The prefix argument toggles whether to search by
+;; regexp or literal string, but it's much more important to be able
+;; to choose a directory interactively.
+;;
+;; So swap 'em. Prolly breaks some assumptions of ack-and-a-half.
+
+(defun pd/ack--dir ()
+  (if current-prefix-arg
+      (let ((ack-and-a-half-prompt-for-directory t))
+        (ack-and-a-half-read-dir))
+    (ack-and-a-half-read-dir)))
+
+(defun pd/ack (pattern &optional directory)
+  (interactive
+   (list (ack-and-a-half-read ack-and-a-half-regexp-search)
+         (pd/ack--dir)))
+  (ack-and-a-half pattern ack-and-a-half-regexp-search directory))
+
+(defun pd/ack-same (pattern &optional directory)
+  (interactive
+   (list (ack-and-a-half-read ack-and-a-half-regexp-search)
+         (pd/ack--dir)))
+  (ack-and-a-half-same pattern ack-and-a-half-regexp-search directory))
+
+(defalias 'ack 'pd/ack)
+(defalias 'ack-same 'pd/ack-same)
