f8b4a6256b8a50b54066b5ebad6f5aa022a3f08d
Kyle Hargraves <pd@krh.me>
2012-12-25 03:41:23 -0600

    moved pd/shell stuff into after-load

--- emacs.d/after-loads/after-shell.el
+++ emacs.d/after-loads/after-shell.el
@@ -1,8 +1,33 @@
 (require 'ansi-color)
 (require 'dirtrack)
 (require 'pcmpl-git)
-(require 'pd/shell)
 (require 'pd/pcmpl-powify)
+
+(defun pd/enable-shell-mode-bindings ()
+  (define-key shell-mode-map (kbd "C-c d")   'dirs)
+  (define-key shell-mode-map (kbd "C-c s")   'pd/smart-shell)
+  (define-key shell-mode-map (kbd "C-c M-s") 'pd/ido-switch-shell)
+  (define-key shell-mode-map (kbd "C-c DEL") 'pd/clear-shell))
+
+; TODO: why isn't this just erase-buffer?
+(defun pd/clear-shell ()
+  "Clear the contents of a buffer without bothering with the kill-ring."
+  (interactive)
+  (delete-region (point-min) (point-max)))
+
+(defun pd/dirtrack-directory-function (path)
+  (pd/expand-file-name path))
+
+(defun pd/dirtrack-directory-change-hook ()
+  (let* ((cwd (directory-file-name (pd/abbreviate-file-name default-directory)))
+         (new-name (concat "*shell: " cwd "*")))
+    (rename-buffer new-name t)))
+
+(defun pd/enable-dirtrack ()
+  (dirtrack-mode 1)
+  (shell-dirtrack-mode -1)
+  (setq dirtrack-list '("\\`%?[\r\n ]*\\([^\n ]+\\) .*» \\'" 1))
+  (setq dirtrack-directory-function 'pd/dirtrack-directory-function))
 
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (add-hook 'shell-mode-hook 'pd/turn-off-show-trailing-whitespace)
