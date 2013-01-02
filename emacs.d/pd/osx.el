6f46c565d495a544beff2da9f3a3e1eced4da818
Kyle Hargraves <pd@krh.me>
2013-01-02 13:12:22 -0600

    default emacs font = monaco 10pt

--- emacs.d/pd/osx.el
+++ emacs.d/pd/osx.el
@@ -12,6 +12,10 @@
 ;; gank the $PATH from a login shell, in case I launched from the dock
 (exec-path-from-shell-initialize)
 
+;; Dunno a better way to do this. Sets the font to (roughly) Monaco 10pt.
+;; Default was 12pt.
+(set-face-attribute 'default nil :height 100)
+
 ;; I launch emacs client using an applescript, which sets cwd to /
 ;; Rather than learn applescript, I fix it here.
 (when (string= default-directory "/")
