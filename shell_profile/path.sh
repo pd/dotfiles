4e276845923ee30bcf2ea6e092da127f63b0c112
Kyle Hargraves <pd@krh.me>
2013-01-25 22:02:10 -0600

    add /usr/local/share/python to path

--- shell_profile/path.sh
+++ shell_profile/path.sh
@@ -6,6 +6,7 @@ add_to_path_if_exists /usr/local/sbin
 add_to_path_if_exists /usr/local/bin
 add_to_path_if_exists /usr/local/texlive/2011/bin/x86_64-darwin
 add_to_path_if_exists /usr/local/heroku/bin
+add_to_path_if_exists /usr/local/share/python
 add_to_path_if_exists ~/.carton/bin
 add_to_path_if_exists ~/dotfiles/vendor/ecukes
 add_to_path_if_exists ~/local/bin
