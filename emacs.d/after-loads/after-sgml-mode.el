00d7ca53c0d9c9641680eb3adf9fe74e2e35ee84
Kyle Hargraves <pd@krh.me>
2013-04-05 12:10:31 -0500

    tagedit, for paredit-ish bindings in html-mode

--- emacs.d/after-loads/after-sgml-mode.el
+++ emacs.d/after-loads/after-sgml-mode.el
@@ -1,5 +1,11 @@
-(require 'zencoding)
+(require 'zencoding-mode)
+(require 'tagedit)
+(tagedit-add-paredit-like-keybindings)
+
 (setq zencoding-insert-flash-time 0.01
       zencoding-indentation 2)
+
 (add-hook 'sgml-mode-hook 'zencoding-mode)
+(add-hook 'html-mode-hook 'tagedit-mode)
+
 (keydef (sgml "C-c e") zencoding-expand-line)
