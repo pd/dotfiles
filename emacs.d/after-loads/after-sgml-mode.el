ca14cb5f0da0d26a24334216ca07be3b6d17628b
Kyle Hargraves <pd@krh.me>
2013-04-05 16:17:32 -0500

    don't let tagedit have s-k

--- emacs.d/after-loads/after-sgml-mode.el
+++ emacs.d/after-loads/after-sgml-mode.el
@@ -1,6 +1,9 @@
 (require 'zencoding-mode)
 (require 'tagedit)
+
 (tagedit-add-paredit-like-keybindings)
+(keydef (tagedit "s-k") windmove-up)
+(keydef (tagedit "s-K") tagedit-kill-attribute)
 
 (setq zencoding-insert-flash-time 0.01
       zencoding-indentation 2)
