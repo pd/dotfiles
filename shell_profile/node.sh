06f1fa5327de1dd8653bae7b19e429ca74db05a5
Kyle Hargraves <pd@krh.me>
2013-04-15 15:42:21 -0500

    modernize NODE_PATH

--- shell_profile/node.sh
+++ shell_profile/node.sh
@@ -1,3 +1,7 @@
 if [ -d /usr/local/lib/node_modules ]; then
   NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
 fi
+
+if [ -d /usr/local/share/npm/lib/node_modules ]; then
+  NODE_PATH="/usr/local/share/npm/lib/node_modules:$NODE_PATH"
+fi
