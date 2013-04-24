9446e8b64aeed2367c2002b88a1314393ab2271f
Kyle Hargraves <pd@krh.me>
2013-04-24 09:42:09 -0500

    C-c e ...: eval-* for lisps

--- emacs.d/after-loads/after-lisp-mode.el
+++ emacs.d/after-loads/after-lisp-mode.el
@@ -2,4 +2,8 @@
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
 
-(define-key lisp-mode-shared-map (kbd "C-c e") 'eval-print-last-sexp)
+(define-key lisp-mode-shared-map (kbd "C-c e b")   'eval-buffer)
+(define-key lisp-mode-shared-map (kbd "C-c e d")   'eval-defun)
+(define-key lisp-mode-shared-map (kbd "C-c e r")   'eval-region)
+(define-key lisp-mode-shared-map (kbd "C-c e s")   'eval-last-sexp)
+(define-key lisp-mode-shared-map (kbd "C-c e M-s") 'eval-print-last-sexp)
