5271f084afcd3c2a8408eb15585c1965cadf17e2
Kyle Hargraves <pd@krh.me>
2013-04-24 09:29:27 -0500

    js: C-c C-r = js2-refactor; node externs; fix M-j

--- emacs.d/after-loads/after-js2-mode.el
+++ emacs.d/after-loads/after-js2-mode.el
@@ -1,23 +1,25 @@
+(require 'slime)
 (require 'slime-js)
-
-(defun pd/run-swank-js ()
-  (interactive)
-  (message "Launching swank-js ...")
-  (apply #'make-comint "swank-js" "swank-js" nil nil))
 
 (defun pd/slime-js-node ()
   (interactive)
   (when (not (get-buffer "*swank-js*"))
-    (pd/run-swank-js))
+    (message "Launching swank-js ...")
+    (apply #'make-comint "swank-js" "swank-js" nil nil)
+    (sleep-for 1))
   (setq slime-protocol-version 'ignore)
   (slime-connect "localhost" 4005))
 
-(defun pd/js2-setup ()
-  (setq js2-basic-offset 2
-        js2-bounce-indent-p nil
-        js2-missing-semi-one-line-override t))
+(setq-default js2-basic-offset 2
+              js2-bounce-indent-p nil
+              js2-missing-semi-one-line-override t
+              js2-include-node-externs t
+              js2-include-browser-externs nil)
 
-(add-hook 'js2-mode-hook 'pd/js2-setup)
 (add-hook 'js2-mode-hook 'slime-js-minor-mode)
 
 (keydef (js2 "C-c x j") 'pd/slime-js-node)
+(keydef (js2 "M-j")     (join-line 1))
+
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(js2r-add-keybindings-with-prefix "C-c r")
--- shell_profile/node.sh
+++ shell_profile/node.sh
@@ -5,3 +5,6 @@ fi
 if [ -d /usr/local/share/npm/lib/node_modules ]; then
   NODE_PATH="/usr/local/share/npm/lib/node_modules:$NODE_PATH"
 fi
+
+alias node='rlwrap node'
+alias tracegl='node ~/vendor/tracegl/tracegl.js -nolib'
