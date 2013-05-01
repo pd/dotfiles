4d35327b3b34daefeeb856a992e21e502fcc8842
Kyle Hargraves <pd@krh.me>
2013-04-30 18:53:05 -0700

    dired-details+ makes creating files work properly

--- emacs.d/after-loads/after-dired.el
+++ emacs.d/after-loads/after-dired.el
@@ -1,5 +1,7 @@
-(require 'dired-details)
+(require 'dired-details+)
+
 (setq-default dired-details-hidden-string "--- ")
 (when (pd/macosx-p)
   (setq dired-use-ls-dired nil))
+
 (dired-details-install)
