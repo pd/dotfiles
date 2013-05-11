83bd5dffa184e1ae17435d7a3092eea0ccc57861
Kyle Hargraves <pd@krh.me>
2013-05-11 09:21:06 -0500

    more snippets

--- emacs.d/after-loads/after-yasnippet.el
+++ emacs.d/after-loads/after-yasnippet.el
@@ -3,3 +3,6 @@
 (define-key yas-minor-mode-map (kbd "C-M-,") 'yas-expand)
 (define-key yas-minor-mode-map (kbd "TAB")   nil)
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
+
+(add-hook 'snippet-mode-hook (lambda ()
+                               (setq mode-require-final-newline nil)))
