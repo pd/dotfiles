42217b06d936cfa0476a06d31eab5d7c829e411b
Kyle Hargraves <pd@krh.me>
2013-06-12 10:16:04 -0500

    better slim & erb configs

--- emacs.d/pd/markup.el
+++ emacs.d/pd/markup.el
@@ -4,10 +4,19 @@
 (after 'scss-mode
   (setq scss-compile-at-save nil))
 
+(after 'slim-mode
+  (add-hook 'slim-mode-hook 'show-paren-mode)
+  (add-hook 'slim-mode-hook 'hl-line-mode)
+  (add-hook 'slim-mode-hook 'pd/show-trailing-whitespace-mode)
+  (add-hook 'slim-mode-hook 'pd/require-final-newline-mode))
+
 (after 'sgml-mode
   (require 'tagedit)
+  (defun pd/maybe-tagedit-mode ()
+    (unless (string-match-p "\\.erb\\'" (buffer-file-name))
+      (tagedit-mode +1)))
   (add-hook 'sgml-mode-hook 'zencoding-mode)
-  (add-hook 'html-mode-hook 'tagedit-mode))
+  (add-hook 'html-mode-hook 'pd/maybe-tagedit-mode))
 
 (after 'tagedit
   (tagedit-add-paredit-like-keybindings)
