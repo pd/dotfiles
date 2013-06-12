3a8fc340857a12a840919645b40788af605f960d
Kyle Hargraves <pd@krh.me>
2013-06-12 10:16:42 -0500

    electric indent sucks in some modes
    in fact it mostly sucks
    
    but it's better than dealing with enabling newline and indent for 9,000
    fkn modes. meh.

--- emacs.d/pd/prog.el
+++ emacs.d/pd/prog.el
@@ -8,11 +8,33 @@
   :lighter nil
   (progn (setq require-final-newline pd/require-final-newline-mode)))
 
+(define-minor-mode pd/electric-indent-incompatible-mode
+  "Disables `electric-indent-mode' in a buffer-local fashion."
+  :lighter nil
+  (progn
+    (if pd/electric-indent-incompatible-mode
+        (progn
+          (set (make-local-variable 'electric-indent-functions)
+               (list (lambda (arg) 'no-indent))))
+      (set (make-local-variable 'electric-indent-functions) nil))))
+
 (add-hook 'prog-mode-hook 'show-paren-mode)
 (add-hook 'prog-mode-hook 'hl-line-mode)
 (add-hook 'prog-mode-hook 'fic-ext-mode)
 (add-hook 'prog-mode-hook 'pd/show-trailing-whitespace-mode)
 (add-hook 'prog-mode-hook 'pd/require-final-newline-mode)
+
+(after 'feature-mode
+  (add-hook 'feature-mode-hook 'pd/electric-indent-incompatible-mode)
+  (bind-key "RET" 'newline-and-indent feature-mode-map))
+
+(after 'coffee-mode
+  (add-hook 'coffee-mode-hook 'pd/electric-indent-incompatible-mode)
+  (bind-key "RET" 'newline-and-indent coffee-mode-map))
+
+(after 'slim-mode
+  (add-hook 'slim-mode-hook 'pd/electric-indent-incompatible-mode)
+  (bind-key "RET" 'newline-and-indent slim-mode-map))
 
 (after 'magit
   (pd/load-ext 'magit)
