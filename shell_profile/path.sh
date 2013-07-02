41777e84e36ab6bdcbd2db61812697517352284c
Kyle Hargraves <pd@krh.me>
2013-07-02 17:46:08 -0500

    rm /usr/local/share/python from path
    so says homebrew:
    
    ~/sauce/cukember » brew doctor
    Warning: /usr/local/share/python is not needed in PATH.
    Formerly homebrew put Python scripts you installed via `pip` or `pip3`
    (or `easy_install`) into that directory above but now it can be removed
    from your PATH variable.
    Python scripts will now install into /usr/local/bin.
    You can delete anything, except 'Extras', from the /usr/local/share/python
    (and /usr/local/share/python3) dir and install affected Python packages
    anew with `pip install --upgrade`.

--- shell_profile/path.sh
+++ shell_profile/path.sh
@@ -6,7 +6,6 @@ add_to_path_if_exists /usr/local/sbin
 add_to_path_if_exists /usr/local/bin
 add_to_path_if_exists /usr/local/texlive/2011/bin/x86_64-darwin
 add_to_path_if_exists /usr/local/heroku/bin
-add_to_path_if_exists /usr/local/share/python
 add_to_path_if_exists ~/.carton/bin
 add_to_path_if_exists /usr/local/share/npm/bin
 add_to_path_if_exists ~/local/bin
