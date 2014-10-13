fe1e40f09cd237b65a725226b47db0762f7e93f9
Kyle Hargraves <pd@krh.me>
2014-10-13 15:03:18 -0500

    i run rubocop a lot these days

--- shell_profile/ruby.sh
+++ shell_profile/ruby.sh
@@ -4,3 +4,6 @@ alias be="bundle exec"
 alias bi="bundle install"
 alias bl="bundle list"
 alias bu="bundle update"
+
+alias rfo="rubocop --format offenses"
+alias ro="rubocop --display-cop-names --format simple --only"
