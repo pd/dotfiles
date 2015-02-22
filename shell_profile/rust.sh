d5453822ed769c7ddb289d66c47548b800483a0b
Kyle Hargraves <pd@krh.me>
2015-02-22 10:36:31 -0600

    $RUST_SRC_PATH, for racer

--- shell_profile/rust.sh
+++ shell_profile/rust.sh
@@ -2,3 +2,6 @@ alias ct='cargo test'
 alias cb='cargo build'
 alias cr='cargo run'
 alias cc='cargo clean' # i don't ever actually call `cc`, this _should_ be fine ...
+
+# for racer
+export RUST_SRC_PATH=$HOME/vendor/rust
