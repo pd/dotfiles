3e1af305c5c5cbe7f3d15b17d9c1210942a81f96
Kyle Hargraves <pd@krh.me>
2015-07-24 16:11:10 -0500

    rm pd/ext/{magit,package}
    the magit stuff is useless as i never work with submodules or signing
    off on other people's commits any more
    
    the package stuff is useless as i use paradox now

--- emacs.d/pd/ext/package.el
+++
@@ -1,7 +0,0 @@
-(require 's)
-
-(defun pd/package-upgrades ()
-  "More useful than the '12 packages can be upgraded' message package.el offers."
-  (interactive)
-  (let ((upgrades (package-menu--find-upgrades)))
-    (when upgrades (message "Outdated: %s" (s-join ", " (mapcar 'symbol-name (mapcar 'car upgrades)))))))
