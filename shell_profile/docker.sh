11d3d19f995cf054c84f8abb335d232c42560868
Kyle Hargraves <pd@krh.me>
2015-07-31 09:58:04 -0500

    DOCKER_HOST needs `tcp://...`

--- shell_profile/docker.sh
+++ shell_profile/docker.sh
@@ -1,5 +1,5 @@
 if which boot2docker >/dev/null 2>&1; then
-  export DOCKER_HOST=127.0.0.1:2376
+  export DOCKER_HOST=tcp://127.0.0.1:2376
   export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
   export DOCKER_TLS_VERIFY=1
 fi
