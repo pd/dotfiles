91023f573672fc3aa4213d25811959a6b6bad342
Kyle Hargraves <pd@krh.me>
2015-07-31 09:58:18 -0500

    alias spacemacs='...'

--- shell_profile/emacs.sh
+++ shell_profile/emacs.sh
@@ -4,3 +4,8 @@ if [ -n "$INSIDE_EMACS" ]; then
 fi
 
 alias em='emacsclient -n'
+
+
+# I like toying with spacemacs, but I'm not actually willing to
+# switch.
+alias spacemacs='/Applications/Emacs.app/Contents/MacOS/Emacs -Q -l ~/spacemacs.d/init.el'
