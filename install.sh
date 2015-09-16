9f69404fbbb3001abfb401d6da49d84b7828153f
Kyle Hargraves <pd@krh.me>
2015-09-15 21:29:41 -0500

    replace Rakefile with install.sh
    clearly i need it once every few years at best cuz there's just no
    reason that should depend on ruby still

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -eu
+
+DOTDIR="${HOME}/dotfiles"
+BINDIR="${HOME}/bin"
+
+noisy_link () {
+  echo "ln -s \"${1}\" \"${2}\""
+  ln -s "${1}" "${2}"
+}
+
+cautious_link () {
+  if [[ -e "${2}" ]]; then
+    echo "skipped: ${2}"
+  else
+    noisy_link "${1}" "${2}"
+  fi
+}
+
+cautious_link_bin () {
+  local name=$(basename "$1")
+  local src="${DOTDIR}/bin/${name}"
+  local dest="${BINDIR}/${name}"
+
+  if [[ ! -d "${src}" ]]; then
+    cautious_link "${src}" "${dest}"
+  fi
+}
+
+clone_bundle () {
+  local repo="${1}"
+  local dir=$(echo $repo | sed 's,.*/,,; s,\.git$,,')
+
+  git clone --depth 1 -- "${repo}" "${DOTDIR}/vim/bundle/${dir}"
+}
+
+for f in zshrc zshenv emacs.d gitconfig vim vimrc vim-tmp pryrc irbrc sqliterc psqlrc; do
+  cautious_link "${HOME}/dotfiles/${f}" "${HOME}/.${f}"
+done
+
+for x in ./bin/*; do
+  cautious_link_bin "${x}"
+done
+
+cat ./vim/bundles.txt | while read b; do
+  clone_bundle "${b}"
+done
