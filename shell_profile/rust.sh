ffc98b88c9df528d6ada27b67735d3c6cf08bff3
Kyle Hargraves <pd@krh.me>
2015-11-02 14:07:29 -0600

    fix $RUST_SRC_PATH

--- shell_profile/rust.sh
+++ shell_profile/rust.sh
@@ -4,4 +4,4 @@ alias cr='cargo run'
 alias cc='cargo clean' # i don't ever actually call `cc`, this _should_ be fine ...
 
 # for racer
-export RUST_SRC_PATH=$HOME/vendor/rust
+export RUST_SRC_PATH=$HOME/vendor/rust/src
