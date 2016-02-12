8ceb51752a2ec223c99a43cbfdd7106474509411
Kyle Hargraves <pd@krh.me>
2016-02-12 12:42:37 -0600

    setq company-tooltip-align-annotations t

--- emacs.d/pd/core.el
+++ emacs.d/pd/core.el
@@ -20,6 +20,7 @@
 
 (after 'company
   (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-tooltip-align-annotations t)
 
   (bind-keys :map company-mode-map
              ("C-<tab>" . company-manual-begin))
