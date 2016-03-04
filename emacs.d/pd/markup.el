c26ddd457955de96977871cc56265defa9c32137
Kyle Hargraves <pd@krh.me>
2016-03-04 12:55:11 -0600

    web-mode 2-space indents

--- emacs.d/pd/markup.el
+++ emacs.d/pd/markup.el
@@ -5,6 +5,9 @@
   (require 'markdown-toc nil 'noerror))
 
 (after 'web-mode
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2)
   (bind-key "C-c e" 'emmet-expand-line web-mode-map))
 
 (provide 'pd/markup)
