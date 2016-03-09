ad15c579731a5b3adca55f3a6a46f7cdef8e2771
Kyle Hargraves <pd@krh.me>
2016-03-09 01:12:00 -0600

    less stupid path.sh

--- shell_profile/path.sh
+++ shell_profile/path.sh
@@ -1,21 +1,23 @@
-add_to_path_if_exists () {
-  [ -d $1 ] && PATH="$1:$PATH"
+prepend_path () {
+  if [[ -d "$1" && ":$PATH:" != *":$1:"* ]]; then
+    PATH="$1:$PATH"
+  fi
 }
 
-add_to_path_if_exists /usr/local/sbin
-add_to_path_if_exists /usr/local/bin
-add_to_path_if_exists /usr/local/texlive/2011/bin/x86_64-darwin
-add_to_path_if_exists /usr/local/opt/postgresql-9.3/bin
-add_to_path_if_exists /usr/local/opt/sqlite/bin
-add_to_path_if_exists /usr/local/heroku/bin
-add_to_path_if_exists /usr/local/share/npm/bin
-add_to_path_if_exists ~/.cask/bin
-add_to_path_if_exists ~/local/bin
-add_to_path_if_exists /opt/chefdk/bin
-add_to_path_if_exists ~/anaconda/bin
-add_to_path_if_exists ~/.multirust/bin
-add_to_path_if_exists ~/.multirust/toolchains/nightly/cargo/bin
-add_to_path_if_exists ~/.multirust/toolchains/stable/cargo/bin
-add_to_path_if_exists /usr/local/opt/go/libexec/bin
-add_to_path_if_exists $GOPATH/bin
-add_to_path_if_exists ~/bin
+prepend_path /usr/local/sbin
+prepend_path /usr/local/bin
+prepend_path /usr/local/texlive/2011/bin/x86_64-darwin
+prepend_path /usr/local/opt/postgresql-9.3/bin
+prepend_path /usr/local/opt/sqlite/bin
+prepend_path /usr/local/heroku/bin
+prepend_path /usr/local/share/npm/bin
+prepend_path ~/.cask/bin
+prepend_path ~/local/bin
+prepend_path /opt/chefdk/bin
+prepend_path ~/anaconda/bin
+prepend_path ~/.multirust/bin
+prepend_path ~/.multirust/toolchains/nightly/cargo/bin
+prepend_path ~/.multirust/toolchains/stable/cargo/bin
+prepend_path /usr/local/opt/go/libexec/bin
+prepend_path $GOPATH/bin
+prepend_path ~/bin
