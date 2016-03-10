da1c9eb00fc99e79dbcc830eb77cedfad53c7f61
Kyle Hargraves <pd@krh.me>
2016-03-10 12:19:54 -0600

    i really hate custom.el

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,9 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#ffffff" "#f36c60" "#8bc34a" "#fff59d" "#4dd0e1" "#b39ddb" "#81d4fa" "#263238"))
+ '(custom-safe-themes
+   (quote
+    ("959a77d21e6f15c5c63d360da73281fdc40db3e9f94e310fc1e8213f665d0278" "e97dbbb2b1c42b8588e16523824bc0cb3a21b91eefd6502879cf5baa1fa32e10" default)))
+ '(fci-rule-color "#37474f")
+ '(hl-sexp-background-color "#1c1f26")
  '(package-selected-packages
    (quote
-    (zoom-frm zerodark-theme zencoding-mode yard-mode yaml-mode wrap-region which-key web-mode vagrant-tramp ujelly-theme theme-looper tagedit subatomic-theme spacemacs-theme smex smart-mode-line-powerline-theme shell-switcher scss-mode sass-mode rust-mode ruby-tools ruby-hash-syntax rspec-mode repl-toggle rainbow-mode quickref projectile-rails phi-search pg pcmpl-git paredit paradox palette noflet nodejs-repl molokai-theme material-theme markdown-mode magit lua-mode less-css-mode kurecolor js2-refactor idomenu ibuffer-vc helm-rubygems-local helm-projectile helm-mode-manager helm-ls-git helm-bind-key helm-ag gotest go-eldoc go-direx gitignore-mode gitconfig-mode git-timemachine fuzzy flx-ido find-file-in-repository feature-mode eyedropper expand-region exec-path-from-shell evil-surround evil-matchit evil-lisp-state evil-easymotion evil-anzu enh-ruby-mode elixir-mode elisp-slime-nav easy-after-load discover dired-details+ diminish dash-at-point darktooth-theme csv-nav csv-mode company-racer company-inf-ruby company-go company-edbi color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode chruby cask bundler buffer-move boron-theme bliss-theme ample-zen-theme ample-theme ag afternoon-theme ace-jump-mode)))
+    (linum-off discover-my-major dracula-theme undercover flycheck-package robe package-build shut-up epl git commander f dash s zoom-frm zerodark-theme zencoding-mode yard-mode yaml-mode wrap-region which-key web-mode vagrant-tramp ujelly-theme theme-looper tagedit subatomic-theme spacemacs-theme smex smart-mode-line-powerline-theme shell-switcher scss-mode sass-mode rust-mode ruby-tools ruby-hash-syntax rspec-mode repl-toggle rainbow-mode quickref projectile-rails phi-search pg pcmpl-git paredit paradox palette noflet nodejs-repl molokai-theme material-theme markdown-mode magit lua-mode less-css-mode kurecolor js2-refactor idomenu ibuffer-vc helm-rubygems-local helm-projectile helm-mode-manager helm-ls-git helm-bind-key helm-ag gotest go-eldoc go-direx gitignore-mode gitconfig-mode git-timemachine fuzzy flx-ido find-file-in-repository feature-mode eyedropper expand-region exec-path-from-shell evil-surround evil-matchit evil-lisp-state evil-easymotion evil-anzu enh-ruby-mode elixir-mode elisp-slime-nav easy-after-load discover dired-details+ diminish dash-at-point darktooth-theme csv-nav csv-mode company-racer company-inf-ruby company-go company-edbi color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode chruby cask bundler buffer-move boron-theme bliss-theme ample-zen-theme ample-theme ag afternoon-theme ace-jump-mode)))
  '(safe-local-variable-values
    (quote
     ((eval when
@@ -16,7 +25,29 @@
            (fboundp
             (quote rainbow-mode))
            (rainbow-mode 1))
-     (encoding . utf-8)))))
+     (encoding . utf-8))))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f36c60")
+     (40 . "#ff9800")
+     (60 . "#fff59d")
+     (80 . "#8bc34a")
+     (100 . "#81d4fa")
+     (120 . "#4dd0e1")
+     (140 . "#b39ddb")
+     (160 . "#f36c60")
+     (180 . "#ff9800")
+     (200 . "#fff59d")
+     (220 . "#8bc34a")
+     (240 . "#81d4fa")
+     (260 . "#4dd0e1")
+     (280 . "#b39ddb")
+     (300 . "#f36c60")
+     (320 . "#ff9800")
+     (340 . "#fff59d")
+     (360 . "#8bc34a"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
