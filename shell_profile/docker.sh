01528346a616890d78c96c3af2aee220ea9b2fe1
Kyle Hargraves <pd@krh.me>
2016-03-15 15:05:51 -0500

    guard against clobbering existing docker env

--- shell_profile/docker.sh
+++ shell_profile/docker.sh
@@ -1,6 +1,8 @@
 if which docker-machine >/dev/null 2>&1; then
-  export DOCKER_TLS_VERIFY="1"
-  export DOCKER_HOST="tcp://192.168.45.128:2376"
-  export DOCKER_CERT_PATH="/Users/khargraves/.docker/machine/machines/dev"
-  export DOCKER_MACHINE_NAME="dev"
+  if [[ -z "${DOCKER_HOST}" ]]; then
+    export DOCKER_TLS_VERIFY="1"
+    export DOCKER_HOST="tcp://192.168.45.128:2376"
+    export DOCKER_CERT_PATH="/Users/khargraves/.docker/machine/machines/dev"
+    export DOCKER_MACHINE_NAME="dev"
+  fi
 fi
