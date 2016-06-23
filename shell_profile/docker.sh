dff2675519e4aedfc3ee3e41a8ab6b7864fec614
Kyle Hargraves <pd@krh.me>
2016-06-23 15:06:17 -0500

    tweak default $DOCKER_HOST

--- shell_profile/docker.sh
+++ shell_profile/docker.sh
@@ -1,7 +1,7 @@
 if which docker-machine >/dev/null 2>&1; then
   if [[ -z "${DOCKER_HOST}" ]]; then
     export DOCKER_TLS_VERIFY="1"
-    export DOCKER_HOST="tcp://192.168.45.128:2376"
+    export DOCKER_HOST="tcp://192.168.99.100:2376"
     export DOCKER_CERT_PATH="/Users/khargraves/.docker/machine/machines/dev"
     export DOCKER_MACHINE_NAME="dev"
   fi
