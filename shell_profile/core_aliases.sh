da0afb6808fbf964cde30718fdc142896790b147
Kyle Hargraves <pd@krh.me>
2016-08-16 12:36:43 -0500

    colorized man pages
    http://boredzo.org/blog/archives/2016-08-15/colorized-man-pages-understood-and-customized

--- shell_profile/core_aliases.sh
+++ shell_profile/core_aliases.sh
@@ -47,3 +47,14 @@ up() {
        fi
   esac
 }
+
+man() {
+  env \
+    LESS_TERMCAP_md=$'\e[1;36m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[1;40;92m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[1;32m' \
+      man "$@"
+}
