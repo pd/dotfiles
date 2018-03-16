de36364d57a3ffa7e63a21e1b763d53351119be3
Kyle Hargraves <pd@krh.me>
2018-03-16 15:33:23 -0500

    drop lots of rust emacs hooks

--- emacs.d/lisp/pd/prog.el
+++ emacs.d/lisp/pd/prog.el
@@ -66,18 +66,10 @@
 
 (after 'rust-mode
   (require 'racer)
-  (require 'flycheck-rust)
-
-  (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
-
   (add-hook 'rust-mode-hook 'subword-mode)
   (add-hook 'rust-mode-hook 'racer-mode)
   (add-hook 'rust-mode-hook 'rust-enable-format-on-save)
-  (add-hook 'rust-mode-hook 'flycheck-mode)
-  (add-hook 'rust-mode-hook 'cargo-minor-mode)
-
   (add-hook 'racer-mode-hook 'eldoc-mode)
-  (add-hook 'racer-mode-hook 'company-mode)
 
   (defun pd/rust-insert-type-error ()
     (interactive)
