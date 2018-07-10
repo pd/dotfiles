aa40ef35b05889e218270bda3cd1f8beedcf7c3e
Kyle Hargraves <pd@krh.me>
2018-07-10 08:24:15 -0500

    Enable vault completion if available

--- shell_profile/completion.sh
+++ shell_profile/completion.sh
@@ -2,3 +2,7 @@ if [[ -f /usr/local/bin/aws_completer ]]; then
   # on zsh, assumes bashcompinit.
   complete -C /usr/local/bin/aws_completer aws
 fi
+
+if [[ -x /usr/local/bin/vault ]]; then
+  complete -o nospace -C /usr/local/bin/vault vault
+fi
