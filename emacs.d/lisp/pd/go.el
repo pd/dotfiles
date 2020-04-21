ca11e3d14f8ac7f5d2bddef2085edc8c5c58c535
Kyle Hargraves <pd@krh.me>
2020-04-21 10:24:05 -0500

    emacs/pd/go: rm legacy oracle and godoc hax

--- emacs.d/lisp/pd/go.el
+++ emacs.d/lisp/pd/go.el
@@ -5,21 +5,9 @@
   (setq tab-width 4)
   (set (make-local-variable 'company-backends) '(company-go)))
 
-(defun pd/enable-go-oracle ()
-  "If $GOPATH is set, and oracle.el is available in it, add `go-oracle-mode'
-to the `go-mode-hook'."
-  (-if-let* ((gopath (getenv "GOPATH"))
-             (oracle-el (f-join gopath "src/golang.org/x/tools/cmd/oracle/oracle.el"))
-             (oracle-exec (executable-find "oracle")))
-      (when (f-exists? oracle-el)
-        (add-to-list 'load-path (f-parent oracle-el))
-        (require 'go-oracle oracle-el)
-        (setq go-oracle-command oracle-exec))))
-
 (after 'go-mode
   (require 'company-go)
   (require 'go-direx)
-  (pd/enable-go-oracle)
 
   (add-hook 'before-save-hook 'gofmt-before-save)
   (add-hook 'go-mode-hook 'go-eldoc-setup)
@@ -36,21 +24,7 @@ to the `go-mode-hook'."
              ("C-c t ." . go-test-current-test)
              ("C-c t t" . go-test-current-file)
              ("C-c t f" . go-test-current-file)
-             ("C-c t p" . go-test-current-project))
-
-  ;; dunno why, but godoc refuses to function despite being in my path:
-  ;;    godoc: zsh:1: command not found: godoc
-  ;; sadly, go-mode does not expose an option to change the command,
-  ;; so i'll just hack that in myself.
-  (defun godoc (query)
-    "Show Go documentation for QUERY, much like M-x man."
-    (interactive (list (godoc--read-query)))
-    (unless (string= query "")
-      (set-process-sentinel
-       (start-process-shell-command "godoc" (godoc--get-buffer query)
-                                    (concat (executable-find "godoc") " " query))
-       'godoc--buffer-sentinel)
-      nil)))
+             ("C-c t p" . go-test-current-project)))
 
 (after 'go-playground
   (bind-key "C-c C-c" 'go-playground-save-and-run go-playground-mode-map)
