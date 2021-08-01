05c5d33309f6371985489f37d6e484f4aed14014
Kyle Hargraves <pd@krh.me>
2021-08-01 09:54:35 -0500

    brew stfu

--- shell_profile/Darwin/darwin.sh
+++ shell_profile/Darwin/darwin.sh
@@ -7,7 +7,12 @@ elif [[ -x "/Applications/MacVim.app/Contents/MacOS/Vim" ]]; then
   alias vim="/Applications/MacVim.app/Contents/MacOS/Vim"
 fi
 
+# https://docs.brew.sh/Manpage#environment
 export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_INSTALL_UPGRADE=1
 
 # Open dash docs from terminal
 dash () {
