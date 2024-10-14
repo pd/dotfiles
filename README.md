de2830a6e10816436ca1425c4e2b91f271dc3d88
Kyle Hargraves <pd@krh.me>
2024-10-14 12:58:38 -0500

    my current digitalocean stack, with state in b2
    DO wants $5/month to use the object store; b2 is just usage based
    billing.
    
    prolly move b2 to its own target later but wfn

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ controls:
 
 1. the NUC media center running arch+kodi
 2. the DO znc/webhost/etc
+
+needs env:
+
+```
+DIGITALOCEAN_TOKEN
+B2_APPLICATION_KEY_ID
+B2_APPLICATION_KEY
+AWS_ACCESS_KEY_ID="$B2_APPLICATION_KEY_ID"
+AWS_SECRET_ACCESS_KEY="$B2_APPLICATION_KEY"
+```
---
+++ do/dns.tf
@@ -0,0 +1,52 @@
+locals {
+  records = {
+    "internetsfamo.us" = [
+      { type = "NS", name = "@", value = "ns1.digitalocean.com." },
+      { type = "NS", name = "@", value = "ns2.digitalocean.com." },
+      { type = "NS", name = "@", value = "ns3.digitalocean.com." },
+      { type = "A", name = "@", value = digitalocean_droplet.do.ipv4_address },
+      { type = "A", name = "www", value = digitalocean_droplet.do.ipv4_address },
+      { type = "CAA", name = "@", value = "letsencrypt.org." },
+    ]
+
+    "krh.me" = [
+      { type = "NS", name = "@", value = "ns1.digitalocean.com." },
+      { type = "NS", name = "@", value = "ns2.digitalocean.com." },
+      { type = "NS", name = "@", value = "ns3.digitalocean.com." },
+
+      { type = "A", name = "do", value = digitalocean_droplet.do.ipv4_address },
+      { type = "A", name = "donix", value = digitalocean_droplet.donix.ipv4_address },
+
+      { type = "CNAME", name = "mail", value = "ghs.googlehosted.com." },
+      { type = "MX", name = "@", value = "aspmx.l.google.com.", priority = 1 },
+      { type = "MX", name = "@", value = "alt1.aspmx.l.google.com.", priority = 5 },
+      { type = "MX", name = "@", value = "alt2.aspmx.l.google.com.", priority = 5 },
+      { type = "MX", name = "@", value = "alt3.aspmx.l.google.com.", priority = 10 },
+      { type = "MX", name = "@", value = "alt4.aspmx.l.google.com.", priority = 10 },
+    ]
+  }
+}
+
+resource "digitalocean_domain" "domains" {
+  for_each = toset(keys(local.records))
+  name     = each.value
+}
+
+resource "digitalocean_record" "dns" {
+  for_each = merge([
+    for domain, records in local.records : {
+      for r in records :
+      "${domain}/${r.type}/${r.name}/${r.value}" => merge(r, { domain = domain })
+    }
+  ]...)
+
+  domain = each.value.domain
+  type   = each.value.type
+  name   = each.value.name
+  value  = each.value.value
+
+  tag   = each.value.type == "CAA" ? "issue" : null
+  flags = each.value.type == "CAA" ? 0 : null
+
+  priority = lookup(each.value, "priority", null)
+}
