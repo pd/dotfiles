5405bb7983594624949daa25accf084e6f75e8d2
Kyle Hargraves <pd@krh.me>
2024-10-24 09:14:14 -0500

    zsh += up, z

--- nixen/hosts/desk/default.nix
+++ nixen/hosts/desk/default.nix
@@ -143,6 +143,11 @@ in
       enableZshIntegration = true;
     };
 
+    programs.autojump = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
     programs.git = {
       enable = true;
       userName = "Kyle Hargraves";
@@ -227,16 +232,32 @@ in
 
     programs.zsh = {
       enable = true;
+
+      enableVteIntegration = true;
       autocd = true;
 
       # Ensure tramp can parse the prompt
       envExtra = ''
         [[ $TERM = "dumb" ]] && unsetopt zle && PS1='$ '
+        export SOPS_AGE_KEY_FILE="$HOME/.age/key-primary-ed25519.txt"
       '';
 
       shellAliases = {
         g = "git";
+        z = "j";
       };
+
+      plugins = [
+        {
+          name = "up";
+          src = pkgs.fetchFromGitHub {
+            owner = "cjayross";
+            repo = "up";
+            rev = "b8f11253753be845aec450c3ac68f317a0c0ec2a";
+            sha256 = "McOiMwLclsZ11PzyvNeMnSK7oiuticpTPKRKb3v8At8=";
+          };
+        }
+      ];
     };
 
   };
