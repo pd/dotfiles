5f1b7da6532d93f532f53bd2eac29dd48fede146
Kyle Hargraves <pd@krh.me>
2024-10-28 00:22:21 -0500

    filebot seems to do 99% of what i want
    $6/year but not auto-renewing, so if I end up sticking with it, this
    will inevitably be something that fails a year from now and I have to
    unravel wtf is going wrong. but better than burning days trying to
    glue together my own tooling, or figure out all the `*arr` tools etc

--- nixen/hosts/htpc/nas.nix
+++ nixen/hosts/htpc/nas.nix
@@ -1,9 +1,16 @@
-{ pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   nasIP = (import ../../modules/net.nix).hosts.nas.lan.ip;
 in
 {
+  sops.secrets.filebot-license = {
+    path = "${config.users.users.pd.home}/.local/share/filebot/data/.license";
+    owner = config.users.users.pd.name;
+    group = config.users.users.pd.group;
+  };
+
   environment.systemPackages = with pkgs; [
+    filebot
     nfs-utils
   ];
 
