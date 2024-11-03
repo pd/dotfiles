be77ff317dede74ba143dcc0eb7b31d30ccc5792
Kyle Hargraves <pd@krh.me>
2024-11-02 20:33:00 -0500

    signal-desktop, age-plugin-yubikey

--- nixen/hosts/desk/home/default.nix
+++ nixen/hosts/desk/home/default.nix
@@ -17,20 +17,24 @@
     home.stateVersion = "24.05";
 
     programs.home-manager.enable = true;
-
     programs.firefox.enable = true;
 
     home.packages = with pkgs; [
       age
+      age-plugin-yubikey
       cfssl
       pkgs-unstable.go
       pkgs-unstable.gotools
       just
       pyrosimple
       screen
+      pkgs-unstable.signal-desktop
       pkgs-unstable.slack
       sops
       watchexec
     ];
   };
+
+  # Needed for age-plugin-yubikey
+  services.pcscd.enable = true;
 }
