1c976267cda57ff7016aab0ec0f3ea3ad064d5d8
Kyle Hargraves <pd@krh.me>
2024-11-06 10:31:32 -0600

    activateSettings after darwin rebuild

--- hosts/span/default.nix
+++ hosts/span/default.nix
@@ -36,6 +36,13 @@
   security.pam.enableSudoTouchIdAuth = true;
 
   system = {
+    activationScripts.postUserActivation.text = ''
+      # activateSettings -u will reload the settings from the database and
+      # apply them to the current session, so we do not need to logout and
+      # login again to make the changes take effect.
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+
     defaults = {
       NSGlobalDomain."com.apple.swipescrolldirection" = false;
       dock = {
