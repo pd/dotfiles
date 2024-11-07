db2205a4c7c4db843c67d29dd9ecd79445d71cf9
Kyle Hargraves <pd@krh.me>
2024-11-07 11:49:21 -0600

    start extracting from span to core

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -56,26 +56,6 @@ in
     vim
   ];
 
-  programs.zsh = {
-    enable = true;
-    interactiveShellInit = ''
-      autoload -U select-word-style
-      select-word-style bash
-
-      if [[ "$TERM" != "dumb" ]]; then
-        source ${pkgs.emacsPackages.vterm}/share/emacs/site-lisp/elpa/*/etc/emacs-vterm-zsh.sh
-        add-zsh-hook -Uz chpwd() {
-          if [[ -n "$SSH_CONNECTION" ]]; then
-            vterm_cmd update-default-directory "/ssh:$(hostname):$PWD/"
-          else
-            vterm_cmd update-default-directory "$PWD/"
-          fi
-          print -Pn "\e]2;%m:%2~\a"
-        }
-      fi
-    '';
-  };
-
   services.openssh = {
     enable = true;
     ports = [ 1222 ];
