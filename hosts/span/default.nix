3713dd854e5b007577eebaa3f2798b8c6ed3fb96
Kyle Hargraves <pd@krh.me>
2024-11-12 09:25:59 -0600

    span: flesh out system.defaults

--- hosts/span/default.nix
+++ hosts/span/default.nix
@@ -36,11 +36,37 @@
     '';
 
     defaults = {
-      NSGlobalDomain."com.apple.swipescrolldirection" = false;
       dock = {
         autohide = true;
         orientation = "bottom";
+        mru-spaces = false;
         show-recents = false;
+      };
+
+      loginwindow = {
+        autoLoginUser = "pd";
+        GuestEnabled = false;
+      };
+
+      menuExtraClock = {
+        Show24Hour = true;
+        ShowDate = 1;
+        ShowDayOfWeek = true;
+        ShowSeconds = false;
+      };
+
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleInterfaceStyleSwitchesAutomatically = false;
+
+        # stop pretending to be a phone
+        "com.apple.swipescrolldirection" = false;
+
+        # no beep when changing volume
+        "com.apple.sound.beep.feedback" = 0;
+
+        # 24h time
+        AppleICUForce24HourTime = true;
       };
     };
 
@@ -48,6 +74,9 @@
       enableKeyMapping = true;
       remapCapsLockToControl = true;
     };
+
+    # shush
+    startup.chime = false;
   };
 
   environment = {
