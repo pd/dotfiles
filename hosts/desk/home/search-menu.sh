9e73a57d5cc14db0b55219b0b62d16891e27ef2a
Kyle Hargraves <pd@krh.me>
2025-06-01 07:25:44 -0500

    search-menu: s/24.11/25.05/

--- hosts/desk/home/search-menu.sh
+++ hosts/desk/home/search-menu.sh
@@ -10,9 +10,9 @@ q="$(searches | fuzzel --dmenu --fuzzy-min-length=2)"
 term="$(echo "$q" | cut -d' ' -f2- | jq -Rr @uri)"
 
 case "$q" in
-  nixos*) url="https://search.nixos.org/options?channel=24.11&query=${term}";;
-  nixpkgs*) url="https://search.nixos.org/packages?channel=24.11&query=${term}";;
-  home-manager*) url="https://home-manager-options.extranix.com/?release=release-24.11&query=${term}";;
+  nixos*) url="https://search.nixos.org/options?channel=25.05&query=${term}";;
+  nixpkgs*) url="https://search.nixos.org/packages?channel=25.05&query=${term}";;
+  home-manager*) url="https://home-manager-options.extranix.com/?release=release-25.05&query=${term}";;
   nixgh*) url="https://github.com/search?type=code&q=language%3Anix%20${term}";;
   nixdarwin*) url="https://searchix.alanpearce.eu/options/darwin/search?query=${term}";;
   *) exit 1;;
