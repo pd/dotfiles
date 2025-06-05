b4b777cd1f651f83aaf9e94de383c23a60221e3a
Kyle Hargraves <pd@krh.me>
2025-06-04 23:14:46 -0500

    donix += packrat

--- hosts/donix/default.nix
+++ hosts/donix/default.nix
@@ -27,4 +27,16 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOZGcHggrgVlMOSh2lG3i8Jp1vA2rz7NyuWSnlVYnUh"
     ];
   };
+
+  users.users.packrat386 = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "keys"
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKykCUxNhZg/CNBEKAb2LPtW4oD6oB1DEeAR55hslr5D"
+    ];
+  };
 }
