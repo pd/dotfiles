5a4a5bc49cd7245f80ee6dc17867c78959c36913
Kyle Hargraves <pd@krh.me>
2025-07-28 20:09:40 -0500

    desk: restore packages lost in home-manager reorg

--- homes/pd@desk/default.nix
+++ homes/pd@desk/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ../pd
@@ -8,4 +8,29 @@
 
   home.stateVersion = "25.05";
   home.homeDirectory = "/home/pd";
+
+  programs.firefox.enable = true;
+
+  home.packages =
+    with pkgs;
+    [
+      opentofu
+      pavucontrol # audio
+      pinta # remedial image editing
+      pyrosimple # rtorrent clis
+      screen
+    ]
+    ++ (with unstable; [
+      go
+      gotools
+      signal-desktop
+      slack
+    ]);
+
+  # connect to orbstack nixos vm by jumping through span
+  programs.ssh.matchBlocks.orb = {
+    proxyJump = "span";
+    hostname = "localhost";
+    port = 32222;
+  };
 }
