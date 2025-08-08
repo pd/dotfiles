497551e327f0c982ed7061eac2651b5b7458993b
Kyle Hargraves <pd@krh.me>
2025-08-08 08:48:32 -0500

    rtorrent-exporter forked to add `size_bytes` metric

--- pkgs/rtorrent-exporter.nix
+++ pkgs/rtorrent-exporter.nix
@@ -1,21 +1,15 @@
-{ fetchurl, stdenv, ... }:
-stdenv.mkDerivation {
+{ fetchFromGitHub, buildGoModule }:
+buildGoModule {
   name = "rtorrent-exporter";
+  version = "";
 
-  src = fetchurl {
-    url = "https://github.com/aauren/rtorrent-exporter/releases/download/v1.4.9/rtorrent-exporter_Linux_amd64.tar.gz";
-    sha256 = "RVFyOIDe02+I7x8fz47Cvp8pKOyNIVCfpEnMi2UXNts=";
+  src = fetchFromGitHub {
+    owner = "pd";
+    repo = "rtorrent-exporter";
+    rev = "size-metric";
+    hash = "sha256-KT4RLWxtrBHWhwwt35E7UO3c7r5MZ7oVSdMSo6VMiNo=";
   };
 
-  sourceRoot = ".";
-
-  phases = [
-    "unpackPhase"
-    "installPhase"
-  ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp rtorrent-exporter-*/rtorrent-exporter $out/bin
-  '';
+  vendorHash = "sha256-xud2r1dDyYv9ImmnhF6sPGtyfpPCPKRIz31Vb1dLq10=";
+  env.CGO_ENABLED = 0;
 }
