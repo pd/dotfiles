7bbfe7e91727089ce02a09c87e1ebf4486bdc021
Kyle Hargraves <pd@krh.me>
2025-08-15 13:08:16 -0500

    waybar-pd: dunst kinda sorta works
    haven't figured out printing utf8 but the basic loop works
    
    gonna rewrite it to use dbus directly when i get a chance

--- pkgs/waybar-pd/src/main.zig
+++ pkgs/waybar-pd/src/main.zig
@@ -9,7 +9,21 @@ const Context = struct {
     seats: std.ArrayList(*wl.Seat) = std.ArrayList(*wl.Seat).init(std.heap.c_allocator),
 };
 
-pub fn main() anyerror!void {
+pub fn main() !void {
+    var args = std.process.args();
+    _ = args.next();
+
+    const mode = args.next() orelse return error.ArgRequired;
+    if (std.mem.eql(u8, mode, "river-mode")) {
+        try riverMode();
+    } else if (std.mem.eql(u8, mode, "dunst")) {
+        try dunst();
+    } else if (std.mem.eql(u8, mode, "idle-inhibit")) {
+        try idleInhibit();
+    }
+}
+
+fn riverMode() !void {
     const display = try wl.Display.connect(null);
     const registry = try display.getRegistry();
 
@@ -27,6 +41,59 @@ pub fn main() anyerror!void {
     while (true) {
         if (display.dispatch() != .SUCCESS)
             return error.DispatchFailed;
+    }
+}
+
+fn dunst() !void {
+    // Should surely use a dbus lib instead but this is the most direct port for now
+    const alloc = std.heap.page_allocator;
+    const argv = [_][]const u8{
+        "dbus-monitor",
+        "--profile",
+        "path='/org/freedesktop/Notifications',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'",
+    };
+
+    var dbusMonitor = std.process.Child.init(&argv, alloc);
+    dbusMonitor.stdout_behavior = .Pipe;
+    try dbusMonitor.spawn();
+
+    const stdout = dbusMonitor.stdout orelse unreachable;
+    var buffered_reader = std.io.bufferedReader(stdout.reader());
+    var line = std.ArrayList(u8).init(alloc);
+
+    while (true) {
+        // We don't actually care what it prints, just that it did
+        line.clearRetainingCapacity();
+        buffered_reader.reader().streamUntilDelimiter(line.writer(), '\n', null) catch |err| switch (err) {
+            error.EndOfStream => {
+                _ = try dbusMonitor.wait();
+                return;
+            },
+            else => return err,
+        };
+
+        // TODO use dbus-send which is all dunstctl is doing?
+        // dbus-send --print-reply=literal --dest=org.freedesktop.Notifications /org/freedesktop/Notifications org.freedesktop.DBus.Properties.Get string:org.dunstproject.cmd0 string:paused
+
+        // for now just call dunstctl itself
+        const dunstArgv = [_][]const u8{ "dunstctl", "is-paused" };
+        const result = try std.process.Child.run(.{
+            .allocator = alloc,
+            .argv = &dunstArgv,
+        });
+
+        const w = std.io.getStdOut().writer();
+        if (std.mem.eql(u8, result.stdout, "true\n")) {
+            w.print("paused\n", .{}) catch return;
+        } else {
+            w.print("not paused\n", .{}) catch return;
+        }
+    }
+}
+
+fn idleInhibit() !void {
+    while (true) {
+        std.time.sleep(5 * std.time.ns_per_s);
     }
 }
 
