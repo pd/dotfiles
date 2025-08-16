5a6ee25870a6ed89f1b8607fdca6a8fe257a294a
Kyle Hargraves <pd@krh.me>
2025-08-16 09:12:20 -0500

    span: Drop emacs-plus from homebrew
    thought i'd already done this, but I guess I just manually uninstalled it
    while testing nix-darwin-emacs and never actually rm'd the code

--- hosts/span/homebrew.nix
+++ hosts/span/homebrew.nix
@@ -4,23 +4,9 @@
     enable = true;
     global.autoUpdate = false;
 
-    taps = [
-      "d12frosted/emacs-plus"
-    ];
-
     caskArgs = {
       appdir = "~/Applications";
     };
-
-    brews = [
-      {
-        name = "emacs-plus@30";
-        args = [
-          "with-native-comp"
-          "with-imagemagick"
-        ];
-      }
-    ];
 
     casks = [
       "alfred"
