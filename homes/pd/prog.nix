fd665c77ba2733a16527bd1e72ee11fa16a94829
Kyle Hargraves <pd@krh.me>
2025-08-20 08:10:33 -0500

    zvm: stop nagging about an update
    hopefully this lands before too long, too:
    
    https://github.com/tristanisham/zvm/pull/117

--- homes/pd/prog.nix
+++ homes/pd/prog.nix
@@ -1,11 +1,22 @@
 { config, pkgs, ... }:
 let
-  ruby' = pkgs.ruby.withPackages (
+  ruby' = pkgs.unstable.ruby.withPackages (
     gems: with gems; [
       pry
       activesupport
     ]
   );
+
+  # use zvm because nixpkgs often has zig+zls diverge, and then most
+  # of zls is hosed. but stop telling me there's an upgrade available
+  zvm = pkgs.symlinkJoin {
+    name = "zvm";
+    paths = [ pkgs.unstable.zvm ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/zvm --set ZVM_SET_CU off
+    '';
+  };
 in
 {
   home.packages = with pkgs.unstable; [
@@ -19,8 +30,6 @@ in
     supabase-cli
     uv
 
-    # use zvm because nixpkgs often has zig+zls diverge,
-    # and then most of zls is hosed
     zvm
   ];
 
