5317f6479273c54a9da54c6146ef4e49f1262671
Kyle Hargraves <pd@krh.me>
2025-09-09 10:18:04 -0500

    htpc: downgrade to pg16
    Need pgvecto-rs extension for immich, but it's currently marked broken
    on pg17:
    
    https://github.com/NixOS/nixpkgs/blob/9d1fa9/pkgs/servers/sql/postgresql/ext/pgvecto-rs/package.nix#L121-L124
    
    I've no real need of anything in 17 atm, so downgrading is the
    simplest fix.

--- hosts/htpc/psql.nix
+++ hosts/htpc/psql.nix
@@ -11,7 +11,7 @@
 
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql_17_jit;
+    package = pkgs.postgresql_16_jit;
 
     ensureDatabases = [ "koito" ];
     ensureUsers = [
