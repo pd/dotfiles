968f97932db3e463dca8f5b1e8f5ca61c7584dd5
Kyle Hargraves <pd@krh.me>
2025-11-11 22:30:37 -0600

    early-init: shush native compilation warnings

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -4,3 +4,10 @@
   (startup-redirect-eln-cache
    (convert-standard-filename
     (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
+; Don't burn 100% CPU just trying to print native compilation
+; errors that I truly don't give a shit about.
+(setq native-comp-async-report-warnings-errors 'silent)
+
+; When that doesn't work out:
+; (setq native-comp-deferred-compilation nil)
