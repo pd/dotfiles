972b78a904c36ced5e1de5326c5c2d0997f9df6a
Kyle Hargraves <pd@krh.me>
2026-03-14 14:17:40 -0500

    home: rm zvm

--- homes/pd/prog.nix
+++ homes/pd/prog.nix
@@ -6,17 +6,6 @@ let
       activesupport
     ]
   );
-
-  # use zvm because nixpkgs often has zig+zls diverge, and then most
-  # of zls is hosed. but stop telling me there's an upgrade available
-  zvm = pkgs.symlinkJoin {
-    name = "zvm";
-    paths = [ pkgs.unstable.zvm ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/zvm --set ZVM_SET_CU off
-    '';
-  };
 in
 {
   home.packages = with pkgs.unstable; [
@@ -32,7 +21,5 @@ in
     (lib.hiPrio ruby') # win over gotools `bundle`
     supabase-cli
     uv
-
-    zvm
   ];
 }
