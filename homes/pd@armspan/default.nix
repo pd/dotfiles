cadbe9ae296044f0e72fd7896bf2f53fc9f0d746
Kyle Hargraves <pd@krh.me>
2026-03-18 10:47:03 -0500

    pd@armspan: nixos-rebuild on darwin

--- homes/pd@armspan/default.nix
+++ homes/pd@armspan/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     ../pd
@@ -9,4 +9,14 @@
   home.stateVersion = "25.05";
 
   programs.ssh.includes = [ "~/.orbstack/ssh/config" ];
+
+  home.packages = [
+    # let me use nixos-rebuild from darwin, keeping the unprefixed
+    # name so i don't have to adjust scripts everywhere
+    (pkgs.symlinkJoin {
+      name = "nixos-rebuild-ng-aliased";
+      paths = [ pkgs.nixos-rebuild-ng ];
+      postBuild = "ln -s $out/bin/nixos-rebuild-ng $out/bin/nixos-rebuild";
+    })
+  ];
 }
