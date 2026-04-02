3e464be2dd676d7e27ae339e0c79d8ed860bb08b
Kyle Hargraves <pd@krh.me>
2026-04-01 22:25:48 -0500

    monitoring/beat: 5s timeout
    gatus or maybe caddy failed to accept a connection in a timely manner,
    curl sat for apparently ~5minutes doing nothing. systemd then misses
    multiple heartbeats, and i get an alert for a machine that's 100% online
    cuz i'm literally using it

--- modules/monitoring/beat.sh
+++ modules/monitoring/beat.sh
@@ -1,6 +1,7 @@
 curl \
   --silent \
   --fail \
+  --max-time 5 \
   --request POST \
   --header "Authorization: Bearer $GATUS_HEARTBEAT_TOKEN" \
   "https://status.krh.me/api/v1/endpoints/heartbeat_$HOSTNAME/external?success=true"
