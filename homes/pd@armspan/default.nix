306da12c74513b774123e0981c99fe37aac6894c
Kyle Hargraves <pd@krh.me>
2026-07-01 08:02:25 -0500

    armspan: drop nixos-rebuild-ng symlink
    it just ships nixos-rebuild now, no need for the postBuild

--- homes/pd@armspan/default.nix
+++ homes/pd@armspan/default.nix
@@ -11,12 +11,6 @@
   programs.ssh.includes = [ "~/.orbstack/ssh/config" ];
 
   home.packages = [
-    # let me use nixos-rebuild from darwin, keeping the unprefixed
-    # name so i don't have to adjust scripts everywhere
-    (pkgs.symlinkJoin {
-      name = "nixos-rebuild-ng-aliased";
-      paths = [ pkgs.nixos-rebuild-ng ];
-      postBuild = "ln -s $out/bin/nixos-rebuild-ng $out/bin/nixos-rebuild";
-    })
+    pkgs.nixos-rebuild-ng
   ];
 }
