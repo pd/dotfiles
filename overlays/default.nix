939179571ce90c6608e38c564ddf095943d95f0f
Kyle Hargraves <pd@krh.me>
2026-07-01 07:51:17 -0500

    overlays: drop unused treesitter override
    `extraGrammars` was dropped, don't feel like looking up a replacement
    since i'm not actually using qmljs grammar anyway

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,22 +16,4 @@
       stevenblack-blocklist = inputs.stevenblack-blocklist;
     };
   };
-
-  # Additional treesitter grammars
-  tree-sitter = _final: prev: {
-    tree-sitter = prev.tree-sitter.override {
-      extraGrammars = {
-        tree-sitter-qmljs = prev.tree-sitter.buildGrammar {
-          language = "qmljs";
-          version = "0889da4";
-          src = prev.fetchFromGitHub {
-            owner = "yuja";
-            repo = "tree-sitter-qmljs";
-            rev = "0889da4632bba3ec6f39ef4102625654890c15c1";
-            sha256 = "sha256-Twj2taG7xFTPXTvzDcWeIqxUAkuhsybwZvtwK/HiruE=";
-          };
-        };
-      };
-    };
-  };
 }
