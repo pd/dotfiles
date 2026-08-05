1f0fb32f6b5ea10002bbdd48681be834429f9f7d
Kyle Hargraves <pd@krh.me>
2026-08-05 09:54:57 -0500

    emacs: evil-surround

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -271,6 +271,10 @@ targets."
   :config
   (require 'evil-cleverparens-text-objects))
 
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode +1))
+
 (use-package evil-textobj-line)
 (use-package evil-textobj-syntax)
 
