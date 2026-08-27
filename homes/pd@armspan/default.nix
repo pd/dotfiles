20436ca47fa6e5f5c9f7a752d6a38fe10750d40b
Kyle Hargraves <pd@krh.me>
2026-08-27 11:17:58 -0500

    armspan: ghostty config, cmd+opt+enter to launch a term

--- homes/pd@armspan/default.nix
+++ homes/pd@armspan/default.nix
@@ -13,4 +13,18 @@
   home.packages = [
     pkgs.nixos-rebuild-ng
   ];
+
+  programs.ghostty = {
+    enable = true;
+    package = null; # via homebrew instead
+    settings = {
+      background = "#101010";
+      unfocused-split-fill = "#5a5a5a";
+      clipboard-paste-protection = false;
+
+      keybind = [
+        "global:cmd+opt+enter=new_window"
+      ];
+    };
+  };
 }
